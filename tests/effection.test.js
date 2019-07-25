31db7e562bd242bd15ebe9f34446e6ee57d0c6d0
Charles Lowell <cowboyd@frontside.io>
2019-07-25 17:55:22 +0300

    Remove defunct effection test

--- tests/effection.test.js
+++
@@ -1,8 +0,0 @@
-import effection from '../index';
-import expect from 'expect';
-
-describe('Effection', () => {
-  it('works', () => {
-    expect(effection()).toEqual('hello effection');
-  });
-});

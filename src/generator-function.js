5683f69d67a4412e3a913ec8e1416613bdea58d2
Jonas Nicklas <jonas@jnicklas.com>
2020-02-24 19:17:56 +0100

    Use constructor name to determine if a function is a generator function
    If we are using a polyfill runtime for generators, the constructors
    might not be identical. Using the name is a bit hacky, but it's the best
    we got.

--- src/generator-function.js
+++ src/generator-function.js
@@ -1,8 +1,5 @@
-const GeneratorFunction = (function*() {}).constructor;
-
-
 export function isGeneratorFunction(fn) {
-  return fn != null && Object.getPrototypeOf(fn) === GeneratorFunction.prototype;
+  return fn && fn.constructor && fn.constructor.name === 'GeneratorFunction';
 }
 
 export function isGenerator(value) {

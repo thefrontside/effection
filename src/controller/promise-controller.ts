14177ec72704d9ec07fd559e42cecca8eb5bbf10
Jonas Nicklas <jonas@jnicklas.com>
2020-05-03 12:40:00 +0200

    Promise cancellation

--- src/controller/promise-controller.ts
+++ src/controller/promise-controller.ts
@@ -1,10 +1,20 @@
 import { Controller } from './controller';
+import { HaltError } from '../halt-error';
 
 export class PromiseController<TOut> implements Controller<TOut> {
-  constructor(private promise: PromiseLike<TOut>) {
+  private promise: PromiseLike<TOut>;
+  private _reject: (error: Error) => void;
+
+  constructor(promise: PromiseLike<TOut>) {
+    this.promise = new Promise((resolve, reject) => {
+      this._reject = reject;
+      promise.then(resolve, reject);
+    });
   }
 
-  halt() {}
+  halt() {
+    this._reject(new HaltError());
+  }
 
   then<TResult1 = TOut, TResult2 = never>(onfulfilled?: ((value: TOut) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
     return this.promise.then(onfulfilled, onrejected);

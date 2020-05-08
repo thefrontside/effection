3b6f17dc2028d08f65b117bcd8eb215fd414d194
Jonas Nicklas <jonas@jnicklas.com>
2020-05-08 13:03:35 +0200

    Can halt generator

--- src/controller/iterator-controller.ts
+++ src/controller/iterator-controller.ts
@@ -1,17 +1,29 @@
 import { Controller } from './controller';
 import { Task } from '../task';
 import { HaltError } from '../halt-error';
+import { Operation } from '../operation';
+
+type Halt = { halted: true }
+
+function isHalt(value: any): value is Halt {
+  return value && value.halted;
+}
 
 export class IteratorController<TOut> implements Controller<TOut> {
-  private currentTask: Task<unknown> | undefined;
   private promise: Promise<TOut>;
+  private haltPromise: Promise<Halt>;
+  private resolveHaltPromise: () => void;
 
   constructor(private iterator: Iterator<unknown>) {
+    this.haltPromise = new Promise((resolve) => {
+      this.resolveHaltPromise = () => { resolve({ halted: true }) };
+    });
+
     this.promise = new Promise(async (resolve, reject) => {
       let resume = (value) => () => this.iterator.next(value);
       let fail = (error) => () => this.iterator.throw(error);
       let halt = () => this.iterator.return();
-      let getNext = resume(undefined);
+      let getNext: (value?: Operation<unknown> | Error | undefined) => IteratorResult<unknown> = resume(undefined);
       while(true) {
         try {
           let next = getNext();
@@ -19,10 +31,18 @@ export class IteratorController<TOut> implements Controller<TOut> {
             resolve(next.value);
             break;
           } else {
-            this.currentTask = new Task(next.value);
+            let subTask: Task<unknown> = new Task(next.value);
             try {
-              let value = await this.currentTask;
-              getNext = resume(value);
+              let wrapper = await Promise.race([
+                subTask.then((value: unknown) => ({ value })),
+                this.haltPromise,
+              ]);
+              if(isHalt(wrapper)) {
+                getNext = halt;
+                reject(new HaltError());
+              } else {
+                getNext = resume(wrapper.value);
+              }
             } catch(error) {
               getNext = fail(error)
             }
@@ -35,9 +55,7 @@ export class IteratorController<TOut> implements Controller<TOut> {
   }
 
   halt() {
-    if(!this.currentTask) {
-      this.currentTask.halt();
-    }
+    this.resolveHaltPromise();
   }
 
   then<TResult1 = TOut, TResult2 = never>(onfulfilled?: ((value: TOut) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
--- test/run.test.ts
+++ test/run.test.ts
@@ -4,26 +4,43 @@ import * as expect from 'expect';
 import { run } from '../src/index';
 
 describe('run', () => {
-  it('runs a promise to completion', () => {
-    let task = run(Promise.resolve(123))
-    expect(task).resolves.toEqual(123);
-  });
-
-  it('can compose multiple promises via generator', () => {
-    let task = run(function*() {
-      let one: number = yield Promise.resolve(12);
-      let two: number = yield Promise.resolve(55);
-      return one + two;
+  describe('with promise', () => {
+    it('runs a promise to completion', () => {
+      let task = run(Promise.resolve(123))
+      expect(task).resolves.toEqual(123);
     });
-    expect(task).resolves.toEqual(67);
+
+    it('can halt a promise', async () => {
+      let promise = new Promise(() => {});
+      let task = run(promise);
+
+      task.halt();
+
+      await expect(task).rejects.toHaveProperty("message", "halted")
+    });
   });
 
-  it('can halt a promise', async () => {
-    let promise = new Promise(() => {});
-    let task = run(promise);
+  describe('with generator', () => {
+    it('can compose multiple promises via generator', () => {
+      let task = run(function*() {
+        let one: number = yield Promise.resolve(12);
+        let two: number = yield Promise.resolve(55);
+        return one + two;
+      });
+      expect(task).resolves.toEqual(67);
+    });
 
-    task.halt();
+    it('can halt generator', async () => {
+      let task = run(function*() {
+        let one: number = yield Promise.resolve(12);
+        let two: number = yield new Promise(() => {});
+        return one + two;
+      });
 
-    await expect(task).rejects.toHaveProperty("message", "halted")
+      task.halt();
+
+      await expect(task).rejects.toHaveProperty("message", "halted")
+    });
   });
+
 });

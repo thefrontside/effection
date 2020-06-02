e7465afc9e8090a7c750850bdc484a5008867a49
Jonas Nicklas <jonas@jnicklas.com>
2020-06-02 17:18:18 +0200

    Simplify controller code with deferred

--- src/controller/promise-controller.ts
+++ src/controller/promise-controller.ts
@@ -1,24 +1,23 @@
 import { Controller } from './controller';
 import { HaltError, swallowHalt } from '../halt-error';
+import { Deferred } from '../deferred';
+
 
 export class PromiseController<TOut> implements Controller<TOut> {
-  private promise: Promise<TOut>;
-  private _reject?: (error: Error) => void;
+  private deferred: Deferred<TOut>;
 
   constructor(promise: PromiseLike<TOut>) {
-    this.promise = new Promise((resolve, reject) => {
-      this._reject = reject;
-      promise.then(resolve, reject);
-    });
+    this.deferred = Deferred();
+    promise.then(this.deferred.resolve, this.deferred.reject);
   }
 
   async halt() {
-    this._reject && this._reject(new HaltError());
+    this.deferred.reject(new HaltError());
 
-    await this.promise.catch(swallowHalt);
+    await this.deferred.promise.catch(swallowHalt);
   }
 
   then<TResult1 = TOut, TResult2 = never>(onfulfilled?: ((value: TOut) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
-    return this.promise.then(onfulfilled, onrejected);
+    return this.deferred.promise.then(onfulfilled, onrejected);
   }
 }

81bb7def92109cbc1fba33212a5e4f3e07442f40
Jonas Nicklas <jonas@jnicklas.com>
2020-09-16 11:36:33 +0200

    Link children

--- src/task.ts
+++ src/task.ts
@@ -2,6 +2,8 @@ import { PromiseController } from './controller/promise-controller';
 import { IteratorController } from './controller/iterator-controller';
 import { Controller } from './controller/controller';
 import { Operation } from './operation';
+import { Deferred } from './deferred';
+import { swallowHalt, isHaltError } from './halt-error';
 
 function isPromise(value: any): value is PromiseLike<unknown> {
   return value && typeof(value.then) === 'function';
@@ -11,10 +13,17 @@ function isGenerator(value: any): value is Iterator<unknown> {
   return value && typeof(value.next) === 'function';
 }
 
+type TaskState = 'running' | 'halting' | 'halted' | 'erroring' | 'errored' | 'completing' | 'completed';
+
 export class Task<TOut> implements PromiseLike<TOut> {
-  private controller: Controller<TOut>;
   private children: Set<Task<unknown>> = new Set();
+  private signal: Deferred<never> = Deferred();
+
+  private controller: Controller<TOut>;
   private promise: Promise<TOut>;
+  private parent?: Task<unknown>;
+
+  public state: TaskState = 'running';
 
   constructor(private operation: Operation<TOut>) {
     if(!operation) {
@@ -31,22 +40,45 @@ export class Task<TOut> implements PromiseLike<TOut> {
     this.promise = this.run();
   }
 
-  private async haltChildren() {
-    await Promise.all(Array.from(this.children).map((c) => c.halt()));
+  private async haltChildren(silent = false) {
+    await Promise.all(Array.from(this.children).map(async (c) => {
+      try {
+        await c.halt();
+      } catch(error) {
+        if(!silent) {
+          throw error;
+        }
+      }
+    }));
   }
 
   private async run(): Promise<TOut> {
     try {
-      let result = await this.controller;
-      return result;
-    } finally {
+      let result = await Promise.race([this.signal.promise, this.controller]);
+      this.state = 'completing';
       await this.haltChildren();
+      this.state = 'completed';
+      return result;
+    } catch(error) {
+      if(isHaltError(error)) {
+        this.state = 'halting';
+        await this.haltChildren();
+        this.state = 'halted';
+        this.parent && this.parent.trapHalt(this as Task<unknown>);
+        throw(error);
+      } else {
+        this.state = 'erroring';
+        await this.haltChildren(true);
+        this.state = 'errored';
+        this.parent && this.parent.trapError(this as Task<unknown>, error);
+        throw(error);
+      }
     }
   }
 
   async halt() {
-    await this.haltChildren();
     await this.controller.halt();
+    await this.then(() => {}, swallowHalt);
   }
 
   then<TResult1 = TOut, TResult2 = never>(onfulfilled?: ((value: TOut) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
@@ -54,8 +86,25 @@ export class Task<TOut> implements PromiseLike<TOut> {
   }
 
   spawn<R>(operation?: Operation<R>): Task<R> {
+    if(this.state !== 'running') {
+      throw new Error('cannot spawn a child on a task which is not running');
+    }
     let child = new Task(operation);
-    this.children.add(child as Task<unknown>);
+    child.link(this as Task<unknown>);
     return child;
+  }
+
+  link(parent: Task<unknown>) {
+    this.parent = parent;
+    parent.children.add(this as Task<unknown>);
+  }
+
+  trapError(child: Task<unknown>, error: Error) {
+    this.signal.reject(error);
+    this.children.delete(child);
+  }
+
+  trapHalt(child: Task<unknown>) {
+    this.children.delete(child);
   }
 }

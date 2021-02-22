b7c07b116546c5fb9b76e8c45e52e1e75145d898
Jonas Nicklas <jonas@jnicklas.com>
2021-02-22 14:50:01 +0100

    Replace promise based suspend with dedicated suspend controller

--- test/run.test.ts
+++ test/run.test.ts
@@ -46,8 +46,22 @@ describe('run', () => {
   });
 
   describe('with undefined', () => {
-    it('suspends indefinitely', () => {
+    it('suspends indefinitely', async () => {
       let task = run();
+
+      await run(sleep(10));
+
+      expect(task.state).toEqual('running');
+    });
+
+    it('can be halted', async () => {
+      let task = run();
+
+      await task.halt();
+
+      await expect(task).rejects.toHaveProperty('message', 'halted')
+      expect(task.state).toEqual('halted');
+      expect(task.result).toEqual(undefined);
     });
   });
 

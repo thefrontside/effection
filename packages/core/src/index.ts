910cc6a67e22e814a98f1f32c776e949b1eae91f
Jonas Nicklas <jonas@jnicklas.com>
2021-02-24 08:46:45 +0100

    Add deprecated function to core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,6 +7,7 @@ export { Task, TaskOptions } from './task';
 export { Operation } from './operation';
 export { sleep } from './sleep';
 export { Effection } from './effection';
+export { deprecated } from './deprecated';
 
 export function run<TOut>(operation?: Operation<TOut>, options?: TaskOptions): Task<TOut> {
   return Effection.root.spawn(operation, options);

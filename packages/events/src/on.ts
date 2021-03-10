d36fe3ee1b7ab639626043997cd3a6d87b89ccd4
Charles Lowell <cowboyd@frontside.io>
2021-03-10 12:01:51 +0200

    Add TSDoc for `on()`, `once()` and friends

--- packages/events/src/on.ts
+++ packages/events/src/on.ts
@@ -2,6 +2,66 @@ import { createStream, Stream } from '@effection/subscription';
 
 import { EventSource, addListener, removeListener } from './event-source';
 
+
+/**
+ * Creates a {@link Stream} from an event source and event name that
+ * produces the event as its next value every time that the source
+ * raises it. Streams created in this way are infinite.
+ *
+ * Both {@link EventTarget} and {@link EventEmitter}
+ * are supported.
+ *
+ * ### Example
+ *
+ * log every click in a web page:
+ *
+ * ```javascript
+ * task.spawn(on(document, 'click').forEach(event => {
+ *   console.log(`click at (${event.pageX}, ${event.pageY})`);
+ * }));
+ * ```
+ *
+ * ### Example
+ *
+ * buffer a node process's stdin into a single string:
+ *
+ * ```javascript
+ * let buffer = '';
+ * task.spawn(on(process.stdin, 'data').forEach(data => {
+ *   buffer += data;
+ * }));
+ */
+export function on<T = unknown>(source: EventSource, name: string): Stream<T, void> {
+  return onEmit<[T]>(source, name).map(([event]) => event)
+}
+
+/**
+ * Exactly like {@link on | on()} except each value produced by the
+ * stream is an {@link Array} of all the arguments passed when the event was
+ * dispatched.
+ *
+ * This should rarely be needed, and never with {@link EventTarget}s
+ * which always invoke their event listeners with a single argument.
+ *
+ * ### Example
+ *
+ * stream an odd-ball event emitter that passes multiple arguments to
+ * its handers. Most event emitters do not act this way:
+ *
+ * ```javascript
+ * let emitter = new EventEmitter();
+ *
+ * task.spawn(onEmit(emitter, 'multiplication').forEach(([left, right]) => {
+ *   console.log(`${left} times ${right} = ${left * right}!`);
+ * }));
+ *
+ * yield sleep(10);
+ *
+ * emitter.emit('multiplication', 7, 6);
+ * //=> 7 times 6 = 42!
+ * ```
+ *
+ */
 export function onEmit<T extends Array<unknown> = unknown[]>(source: EventSource, name: string): Stream<T, void> {
   return createStream((publish) => function*() {
     let listener = (...args: T) => publish(args);
@@ -12,9 +72,4 @@ export function onEmit<T extends Array<unknown> = unknown[]>(source: EventSource
       removeListener(source, name, listener);
     }
   });
-}
-
-
-export function on<T = unknown>(source: EventSource, name: string): Stream<T, void> {
-  return onEmit<[T]>(source, name).map(([event]) => event)
 }

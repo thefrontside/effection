a40776c0efd83db2130bb59d36646c299ee67634
Jonas Nicklas <jonas@jnicklas.com>
2021-03-30 11:46:27 +0200

    Improve subscriptions
    Since adding streams, we have been unsure of what to do with
    subscriptions. Should we double down on streams and get rid of
    subscriptions all together? Should we improve the subscription
    interface? What are the fundamental primitives.
    
    This is an experiment in a new direction, based on some experience
    gained in porting code inside BigTest.
    
    A few key insights:
    
    - For many interfaces, using a Stream is not a good choice, since the
    moment of subscription is crucial and missing any messages is
    unacceptable. Using streams places a high burden on the consumer to
    subscribe (or buffer) at the right point in time. As such,
    subscriptions/iterators are indispensible, and relying on streams only
    is not tenable.
    - Higher order operations (filter/map/match/reduce) are useful on
    streams, but not so much on subscriptions. There is some very limited
    utility for `map`, but I'm interested in seeing if we can get by without
    *any* higher order operations, since it seems odd to provide `filter`
    but not `map`.
    - The fundamental primitive that implements Subscription is a queue.
    Currently, the `createOperationIterator` function uses a queue
    internally, and this is no accident. The ability to push in values on
    one end and receive them on the other is the fundamendal ability that
    defines a Subscription. However while such a primitive exists for
    `Stream` in the form of `Channel`, we currently do not have an
    equivalent for `Subscription`.
    
    This PR attempts to implement these insights.

--- packages/subscription/src/index.ts
+++ packages/subscription/src/index.ts
@@ -2,7 +2,6 @@ export { createOperationIterator } from './create-operation-iterator';
 export { SymbolOperationIterable } from './symbol-operation-iterable';
 export { OperationIterable } from './operation-iterable';
 export { OperationIterator } from './operation-iterator';
-export { Subscription } from './subscription';
+export { createSubscription, Subscription } from './subscription';
 export { createStream, Stream, StringBufferStream } from './stream';
 export { Writable, WritableStream } from './writable-stream';
-export { subscribe } from './subscribe';
--- packages/subscription/src/subscription.ts
+++ packages/subscription/src/subscription.ts
@@ -1,102 +1,83 @@
 import { Operation, Task } from '@effection/core';
-import { DeepPartial, matcher } from './match';
 import { OperationIterator } from './operation-iterator';
-import { OperationIterable } from './operation-iterable';
-import { SymbolOperationIterable } from './symbol-operation-iterable';
 import { Callback, createOperationIterator } from './create-operation-iterator';
 
-export class Subscription<T, TReturn = undefined> implements OperationIterator<T, TReturn> {
-  constructor(private iterator: OperationIterator<T, TReturn>) {}
-
-  static create<T, TReturn>(task: Task<unknown>, callback: Callback<T,TReturn>): Subscription<T,TReturn> {
-    return new Subscription(createOperationIterator(task, callback));
-  }
-
-  static of<T, TReturn>(task: Task<unknown>, iterable: OperationIterable<T, TReturn>): Subscription<T, TReturn> {
-    let iterator = iterable[SymbolOperationIterable](task);
-
-    return new Subscription(iterator);
-  }
-
-  filter(predicate: (value: T) => boolean): Subscription<T, TReturn> {
-    let { iterator } = this;
-    return new Subscription({
-      next(): Operation<IteratorResult<T,TReturn>> {
-        return function*() {
-          while(true) {
-            let result = yield iterator.next();
-            if(result.done) {
-              return result;
-            } else if(predicate(result.value)) {
-              return result;
-            }
-          }
-        }
-      }
-    });
-  }
-
-  match(reference: DeepPartial<T>): Subscription<T,TReturn> {
-    return this.filter(matcher(reference));
-  }
-
-  map<R>(mapper: (value: T) => R): Subscription<R, TReturn> {
-    let { iterator } = this;
-    return new Subscription({
-      next() {
-        return function*() {
-          while(true) {
-            let result = yield iterator.next();
-            if(result.done) {
-              return result;
-            } else {
-              return { done: false, value: mapper(result.value) };
-            }
-          }
-        }
-      }
-    });
-  }
-
-  first(): Operation<T | undefined> {
-    let { iterator } = this;
-    return function*() {
-      let result: IteratorResult<T,TReturn> = yield iterator.next();
-      if(result.done) {
-        return undefined;
-      } else {
-        return result.value;
-      }
-    }
-  }
-
-  expect(): Operation<T> {
-    let { iterator } = this;
-    return function*() {
-      let result: IteratorResult<T,TReturn> = yield iterator.next();
-      if(result.done) {
-        throw new Error('expected subscription to contain a value');
-      } else {
-        return result.value;
-      }
-    }
-  }
-
-  forEach(visit: (value: T) => Operation<void>): Operation<TReturn> {
-    let { iterator } = this;
-    return function*() {
-      while (true) {
-        let result: IteratorResult<T,TReturn> = yield iterator.next();
-        if(result.done) {
-          return result.value;
-        } else {
-          yield visit(result.value);
-        }
-      }
-    }
-  }
-
-  next() {
-    return this.iterator.next();
-  }
+export interface Subscription<T, TReturn = undefined> extends OperationIterator<T, TReturn> {
+  first(): Operation<T | undefined>;
+  expect(): Operation<T>;
+  forEach(visit: (value: T) => (Operation<void> | void)): Operation<TReturn>;
+  join(): Operation<TReturn>;
+  collect(): Operation<Iterator<T, TReturn>>;
+  toArray(): Operation<T[]>;
 }
+
+export function createSubscription<T, TReturn = undefined>(task: Task<unknown>, callback: Callback<T, TReturn>): Subscription<T, TReturn> {
+  let iterator = createOperationIterator(task, callback);
+
+  let subscription = {
+    first(): Operation<T | undefined> {
+      return function*() {
+        let result: IteratorResult<T, TReturn> = yield iterator.next();
+        if(result.done) {
+          return undefined;
+        } else {
+          return result.value;
+        }
+      }
+    },
+
+    expect(): Operation<T> {
+      return function*() {
+        let result: IteratorResult<T, TReturn> = yield iterator.next();
+        if(result.done) {
+          throw new Error('expected to contain a value');
+        } else {
+          return result.value;
+        }
+      }
+    },
+
+    forEach(visit: (value: T) => (Operation<void> | void)): Operation<TReturn> {
+      return function*() {
+        while (true) {
+          let result: IteratorResult<T,TReturn> = yield iterator.next();
+          if(result.done) {
+            return result.value;
+          } else {
+            let operation = visit(result.value);
+            if(operation) {
+              yield operation;
+            }
+          }
+        }
+      }
+    },
+
+    next() {
+      return iterator.next();
+    },
+
+    join(): Operation<TReturn> {
+      return subscription.forEach(() => { /* no op */ });
+    },
+
+    collect(): Operation<Iterator<T, TReturn>> {
+      return function*() {
+        let items: T[] = [];
+        let result = yield subscription.forEach((item) => function*() { items.push(item); });
+        return (function*() {
+          yield *items;
+          return result;
+        })();
+      }
+    },
+
+    toArray(): Operation<T[]> {
+      return function*() {
+        return Array.from<T>(yield subscription.collect());
+      }
+    },
+  };
+
+  return subscription;
+};

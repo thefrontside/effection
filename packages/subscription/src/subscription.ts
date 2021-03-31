3db7270d4ffa10df6570be6d27d3c8d84fa15690
Jonas Nicklas <jonas@jnicklas.com>
2021-03-31 14:49:00 +0200

    Make Queue a first class citizen
    The fundamental primitive which implements the subscription interface is
    `Queue`. This realization means that we should make queue a first class
    citizen.  This makes `createQueue` and the `Queue` interface, makes
    `Queue` implement `Subscription` and makes it closable.
    
    We keep `createSubscription` for parity with Stream, but it is simply
    implemented on top of `Queue`.
    
    For now we will keep the `OperationIterator` and `OperationIterable`
    primitives, but I do not see them really pulling their weight, since
    using rich objects is so much more convenient. In the future we might
    want to get rid of these as well.

--- packages/subscription/src/subscription.ts
+++ packages/subscription/src/subscription.ts
@@ -1,6 +1,8 @@
 import { Operation, Task } from '@effection/core';
 import { OperationIterator } from './operation-iterator';
-import { Callback, createOperationIterator } from './create-operation-iterator';
+import { createQueue } from './index';
+
+type Callback<T,TReturn> = (publish: (value: T) => void) => Operation<TReturn>;
 
 export interface Subscription<T, TReturn = undefined> extends OperationIterator<T, TReturn> {
   first(): Operation<T | undefined>;
@@ -12,72 +14,10 @@ export interface Subscription<T, TReturn = undefined> extends OperationIterator<
 }
 
 export function createSubscription<T, TReturn = undefined>(task: Task<unknown>, callback: Callback<T, TReturn>): Subscription<T, TReturn> {
-  let iterator = createOperationIterator(task, callback);
-
-  let subscription = {
-    first(): Operation<T | undefined> {
-      return function*() {
-        let result: IteratorResult<T, TReturn> = yield iterator.next();
-        if(result.done) {
-          return undefined;
-        } else {
-          return result.value;
-        }
-      }
-    },
-
-    expect(): Operation<T> {
-      return function*() {
-        let result: IteratorResult<T, TReturn> = yield iterator.next();
-        if(result.done) {
-          throw new Error('expected to contain a value');
-        } else {
-          return result.value;
-        }
-      }
-    },
-
-    forEach(visit: (value: T) => (Operation<void> | void)): Operation<TReturn> {
-      return function*() {
-        while (true) {
-          let result: IteratorResult<T,TReturn> = yield iterator.next();
-          if(result.done) {
-            return result.value;
-          } else {
-            let operation = visit(result.value);
-            if(operation) {
-              yield operation;
-            }
-          }
-        }
-      }
-    },
-
-    next() {
-      return iterator.next();
-    },
-
-    join(): Operation<TReturn> {
-      return subscription.forEach(() => { /* no op */ });
-    },
-
-    collect(): Operation<Iterator<T, TReturn>> {
-      return function*() {
-        let items: T[] = [];
-        let result = yield subscription.forEach((item) => function*() { items.push(item); });
-        return (function*() {
-          yield *items;
-          return result;
-        })();
-      }
-    },
-
-    toArray(): Operation<T[]> {
-      return function*() {
-        return Array.from<T>(yield subscription.collect());
-      }
-    },
-  };
-
-  return subscription;
-};
+  let queue = createQueue<T, TReturn>();
+  task.spawn(function*() {
+    let result = yield callback(queue.send);
+    queue.closeWith(result);
+  });
+  return queue.subscription;
+}

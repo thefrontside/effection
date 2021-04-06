aa1a71bf2b46fd2d18994b16fb66cebebadddaa9
Jonas Nicklas <jonas@jnicklas.com>
2021-04-06 11:03:05 +0200

    Remove `createSubscription`
    since it's really just `createStream(...).subscribe(scope)`

--- packages/subscription/src/index.ts
+++ packages/subscription/src/index.ts
@@ -1,7 +1,7 @@
 export { SymbolOperationIterable } from './symbol-operation-iterable';
 export { OperationIterable } from './operation-iterable';
 export { OperationIterator } from './operation-iterator';
-export { createSubscription, Subscription } from './subscription';
+export { Subscription } from './subscription';
 export { createStream, Stream, StringBufferStream } from './stream';
 export { Writable, WritableStream } from './writable-stream';
 export { Queue, createQueue } from './queue';

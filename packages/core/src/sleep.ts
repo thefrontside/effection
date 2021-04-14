58b8beaa850341e087665932235aaa5f681a455d
Charles Lowell <cowboyd@frontside.io>
2021-04-14 14:32:42 +0300

    Simplify sleep function

--- packages/core/src/sleep.ts
+++ packages/core/src/sleep.ts
@@ -1,10 +1,10 @@
 import { Operation } from './operation';
 
 export function sleep(duration: number): Operation<void> {
-  return (task) => ({
+  return {
     perform(resolve) {
       let timeoutId = setTimeout(resolve, duration);
-      task.ensure(() => clearTimeout(timeoutId));
+      return () => clearTimeout(timeoutId);
     }
-  })
+  }
 }

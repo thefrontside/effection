21de51abb3edaabb92e244e89d9dfb98405be36d
Jonas Nicklas <jonas@jnicklas.com>
2021-05-04 13:31:40 +0200

    Fix race combinator

--- packages/core/src/race.ts
+++ packages/core/src/race.ts
@@ -1,15 +1,17 @@
 import type { Operation } from './operation';
 
 export function race<T>(operations: Operation<T>[]): Operation<T> {
-  return scope => (resolve, reject) => {
-    for (let operation of operations) {
-      scope.spawn(function*() {
-        try {
-          resolve(yield operation);
-        } catch (error) {
-          reject(error);
-        }
-      })
+  return (scope) => ({
+    perform: (resolve, reject) => {
+      for (let operation of operations) {
+        scope.spawn(function*() {
+          try {
+            resolve(yield operation);
+          } catch (error) {
+            reject(error);
+          }
+        })
+      }
     }
-  }
+  });
 }

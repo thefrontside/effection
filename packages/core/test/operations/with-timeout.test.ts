572bbcf56c4f3342c6e4bcaa4997b291aec70b15
Jonas Nicklas <jonas@jnicklas.com>
2021-05-04 11:24:39 +0200

    Add timeout and withTimeout

---
+++ packages/core/test/operations/with-timeout.test.ts
@@ -0,0 +1,25 @@
+import '../setup';
+import { describe, it } from 'mocha';
+import * as expect from 'expect';
+
+import { run, sleep, withTimeout } from '../../src/index';
+
+describe('withTimeout', () => {
+  it('completes normally when completed before timeout', async () => {
+    let root = run(withTimeout(10, function*() {
+      yield sleep(5);
+      return "foo"
+    }));
+
+    await expect(root).resolves.toEqual("foo");
+  });
+
+  it('throws an error after the given amount of time', async () => {
+    let root = run(withTimeout(5, function*() {
+      yield sleep(10);
+      return "foo"
+    }));
+
+    await expect(root).rejects.toHaveProperty('message', 'timed out after 5ms');
+  });
+});

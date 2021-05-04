8ae1b4165ec9e8a033d953c77981d6f372121563
Jonas Nicklas <jonas@jnicklas.com>
2021-05-04 10:49:06 +0200

    Sleep without an argument suspends indefinitely

--- packages/core/src/operations/sleep.ts
+++ packages/core/src/operations/sleep.ts
@@ -1,10 +1,12 @@
 import { Operation } from '../operation';
 
-export function sleep(duration: number): Operation<void> {
-  return {
-    perform(resolve) {
-      let timeoutId = setTimeout(resolve, duration);
-      return () => clearTimeout(timeoutId);
+export function sleep(duration?: number): Operation<void> {
+  if(duration) {
+    return {
+      perform(resolve) {
+        let timeoutId = setTimeout(resolve, duration);
+        return () => clearTimeout(timeoutId);
+      }
     }
   }
 }

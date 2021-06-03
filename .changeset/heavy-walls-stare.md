0b6f172d9d86b58dc4de1e02907033e18e4070c9
Jonas Nicklas <jonas@jnicklas.com>
2021-06-03 11:58:47 +0200

    WebsocketServer can accept HTTP server and port can be omitted

---
+++ .changeset/heavy-walls-stare.md
@@ -0,0 +1,5 @@
+---
+"@effection/websocket-server": minor
+---
+
+Can accept an HTTP server and use random port if none given
--- packages/websocket-server/test/websocket.test.ts
+++ packages/websocket-server/test/websocket.test.ts
@@ -1,25 +1,52 @@
 import { describe, it, beforeEach } from '@effection/mocha';
 import * as expect from 'expect'
-import { spawn } from '@effection/core';
+import { createServer } from 'http'
+import { AddressInfo } from 'net'
+import { spawn, ensure } from '@effection/core';
+import { once } from '@effection/events';
 
 import { createWebSocketClient } from '@effection/websocket-client';
-import { createWebSocketServer, WebSocketServer } from '../src/index';
+import { createWebSocketServer, createWebSocketSubscription, WebSocketServer } from '../src/index';
 
 type Message = { value: string };
 
 describe("createWebSocketServer()", () => {
-  beforeEach(function*() {
-    let server: WebSocketServer<Message> = yield createWebSocketServer(47000);
+  it('can send and receive messages', function*() {
+    let server: WebSocketServer<Message> = yield createWebSocketServer();
 
     yield spawn(server.forEach(function*(connection) {
       yield connection.forEach(({ value }) => function*() {
         yield connection.send({ value: value.toUpperCase() });
       });
     }));
+
+    let client = yield createWebSocketClient<Message>(`ws://localhost:${server.port}`);
+
+    yield client.send({ value: 'hello' });
+    yield client.send({ value: 'world' });
+
+    expect(yield client.expect()).toEqual({ value: 'HELLO' });
+    expect(yield client.expect()).toEqual({ value: 'WORLD' });
   });
 
-  it('can send and receive messages', function*() {
-    let client = yield createWebSocketClient<Message>('ws://localhost:47000');
+  it('can provide own http server', function*() {
+    let http = createServer();
+
+    let server: WebSocketServer<Message> = yield createWebSocketSubscription({ http });
+
+    http.listen();
+    yield ensure(() => { http.close(); });
+    yield once(http, 'listening');
+
+    let port = (http.address() as AddressInfo).port;
+
+    yield spawn(server.forEach(function*(connection) {
+      yield connection.forEach(({ value }) => function*() {
+        yield connection.send({ value: value.toUpperCase() });
+      });
+    }));
+
+    let client = yield createWebSocketClient<Message>(`ws://localhost:${port}`);
 
     yield client.send({ value: 'hello' });
     yield client.send({ value: 'world' });

2c9ecd55442c3d67181a0a6ecb7daaa18c6dd39c
minkimcello <minkimcello@gmail.com>
2021-07-15 07:14:03 -0400

    Set redirect and base url for api docs to v2

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Handlebars = require('handlebars');
 
-const BASE_URL="https://mk-ci-4-api--effection-api.netlify.app/";
+const BASE_URL="https://v2--effection-api.netlify.app/"; // for css
 
 Handlebars.registerHelper('relativeURL', function(url) {
   return BASE_URL + url;

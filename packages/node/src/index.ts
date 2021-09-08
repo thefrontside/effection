ad2cc7499038171e2dfd0b65499dbd903d501447
Jonas Nicklas <jonas@jnicklas.com>
2021-09-08 16:13:44 +0200

    Remove node package

--- packages/node/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@effection/process';
-export * from '@effection/main';
-
-console.warn('@effection/node is deprecated, use `@effection/process` for process management and import `main` from the `effection` or `@effection/main` package instead');

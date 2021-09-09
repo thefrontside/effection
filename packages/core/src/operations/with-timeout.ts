32ef84d95d9f646cfc23bc5aa0d0ee676347e789
Jonas Nicklas <jonas@jnicklas.com>
2021-09-09 14:37:05 +0200

    Enable resource support for higher order operations
    This enables `all`, `race` and `withTimeout` to work properly with
    resources.

--- packages/core/src/operations/with-timeout.ts
+++ packages/core/src/operations/with-timeout.ts
@@ -1,11 +1,11 @@
 import { Operation } from '../operation';
 import { timeout } from './timeout';
-import { spawn } from './spawn';
-import { withLabels } from '../labels';
+import { race } from './race';
+import { setLabels } from '../labels';
 
 export function withTimeout<T>(duration: number, operation: Operation<T>): Operation<T> {
-  return withLabels(function*() {
-    yield spawn(timeout(duration));
-    return yield operation;
-  }, { name: 'withTimeout', duration });
+  return setLabels(
+    race([timeout(duration) as Operation<T>, operation]),
+    { name: 'withTimeout', duration }
+  );
 }

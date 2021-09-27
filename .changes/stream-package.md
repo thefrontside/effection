803ed662c09444c0112ffa89d6370f7dd1236bbe
Jonas Nicklas <jonas@jnicklas.com>
2021-09-27 18:03:49 +0200

    Split `Stream` into its own package
    Splits off `Stream` from the subscription package into its own
    `@effection/stream` package.
    
    This just makes more sense from an organizational perspective.

---
+++ .changes/stream-package.md
@@ -0,0 +1,11 @@
+---
+"@effection/subscription": "minor"
+"@effection/atom": "minor"
+"@effection/channel": "minor"
+"@effection/duplex-channel": "minor"
+"@effection/events": "minor"
+"@effection/process": "minor"
+"effection": "minor"
+---
+
+Split off `Stream` from subscription package into its own `@effection/stream` package
--- packages/subscription/src/index.ts
+++ packages/subscription/src/index.ts
@@ -1,7 +1,3 @@
-export { SymbolOperationIterable } from './symbol-operation-iterable';
-export { OperationIterable } from './operation-iterable';
 export { OperationIterator } from './operation-iterator';
 export { Subscription } from './subscription';
-export { createStream, Stream, StringBufferStream } from './stream';
-export { Writable, WritableStream } from './writable-stream';
 export { Queue, createQueue } from './queue';

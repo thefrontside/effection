55733a09a5602fcea74470c90cb1fdcb6eeda448
Jonas Nicklas <jonas@jnicklas.com>
2021-09-30 16:19:09 +0200

    Document atom package

--- packages/atom/src/types.ts
+++ packages/atom/src/types.ts
@@ -1,11 +1,51 @@
 import { Stream } from '@effection/stream';
 
+/**
+ * A slice represents a piece of the atom. Each part of the state can be
+ * updated and retrieved. Any update will cause the entire state of the atom to
+ * be updated.
+ */
 export interface Slice<S> extends Stream<S> {
+  /**
+   * Retrieve the current value of the slice
+   */
   get(): S;
+  /**
+   * Set the value of the slice to the given value
+   */
   set(value: S): void;
+  /**
+   * Update the value of the slice with the given function. The function takes
+   * the current value as an argument and should return the new value.
+   *
+   * @param fn a function which updates the state
+   */
   update(fn: (state: S) => S): void;
+  /**
+   * Return a new {@link Slice} for the given path.
+   */
   slice: MakeSlice<S>;
+  /**
+   * Remove this slice from its parent
+   */
   remove(): void;
+  /**
+   * Return a stream for this slice. Convenient for destructuring assignment.
+   *
+   * ### Example
+   *
+   * ```typescript
+   * import { main } from 'effection';
+   * import { createAtom } from '@effection/atom';
+   *
+   * main(function*() {
+   *   let { stream, set } = createAtom(0);
+   *
+   *   yield spawn(stream.forEach((number) => console.log(number)));
+   *   set(1);
+   * });
+   * ```
+   */
   stream: Stream<S>;
 }
 

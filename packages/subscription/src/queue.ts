41101923824849238a462969e5be3945f2fcc139
Jonas Nicklas <jonas@jnicklas.com>
2021-10-04 14:59:10 +0200

    Add documentation for subscription

--- packages/subscription/src/queue.ts
+++ packages/subscription/src/queue.ts
@@ -3,18 +3,107 @@ import { Subscription } from './index';
 
 type Close<T> = (...args: T extends undefined ? [] : [T]) => void;
 
-type Waiter<T, TReturn> = (value: IteratorResult<T, TReturn>) => void;
+type Waiter<T, TClose> = (value: IteratorResult<T, TClose>) => void;
 
-export interface Queue<T, TReturn = undefined> extends Subscription<T, TReturn> {
+/**
+ * A queue which can act as a subscription. It can be sent messages to and
+ * which can be closed.
+ *
+ * @typeParam T the type of the items in the queue
+ * @typeParam TClose the type of the value that the queue is closed with
+ */
+export interface Queue<T, TClose = undefined> extends Subscription<T, TClose> {
+  /**
+   * Send a value to the queue.
+   */
   send(value: T): void;
-  close: Close<TReturn>;
-  closeWith(value: TReturn): void;
-  subscription: Subscription<T, TReturn>;
+
+  /**
+   * Close the queue
+   *
+   * ### Example
+   *
+   * Closing queue with no close value:
+   *
+   * ```typescript
+   * import { main, createQueue } from 'effection';
+   *
+   * main(function*() {
+   *   let queue = createQueue<number>();
+   *   queue.close();
+   *   yield queue.join();
+   * });
+   * ```
+   *
+   * Closing queue with close value:
+   *
+   * ```typescript
+   * import { main, createQueue } from 'effection';
+   *
+   * main(function*() {
+   *   let queue = createQueue<number, string>();
+   *   queue.close("I'm done");
+   *   let value = yield queue.join();
+   *   console.log(value) // => "I'm done"
+   * });
+   * ```
+   */
+  close: Close<TClose>;
+
+  /**
+   * Like {@link close}, but with no special case for closing a queue without a
+   * value, this makes `closeWith` easier to use from generic code.
+   */
+  closeWith(value: TClose): void;
+
+  /**
+   * Return a subscription for this queue. Useful when using destructuring
+   * assignment.
+   *
+   * ### Example
+   *
+   * ```typescript
+   * import { main, createQueue } from 'effection';
+   *
+   * main(function*() {
+   *   let { send, subscription } = createQueue<number>();
+   *   send(1);
+   *   send(2);
+   *   send(3);
+   *
+   *   yield subscription.forEach((value) => console.log("got number", value));
+   * });
+   * ```
+   */
+  subscription: Subscription<T, TClose>;
 }
 
-export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TReturn> {
-  let waiters: Waiter<T, TReturn>[] = [];
-  let values: IteratorResult<T, TReturn>[] = [];
+/**
+ * Creates a new queue. Queues are unlimited in size and sending a message to a
+ * queue is always synchronous.
+ *
+ * ### Example
+ *
+ * ```typescript
+ * import { main, createQueue } from 'effection';
+ *
+ * main(function*() {
+ *   let queue = createQueue<number>();
+ *   queue.send(1);
+ *   queue.send(2);
+ *   queue.send(3);
+ *
+ *   yield queue.forEach((value) => console.log("got number", value));
+ * });
+ * ```
+ *
+ * @param name the name of the returned subscription, useful for debugging
+ * @typeParam T the type of the items in the queue
+ * @typeParam TClose the type of the value that the queue is closed with
+ */
+export function createQueue<T, TClose = undefined>(name = 'queue'): Queue<T, TClose> {
+  let waiters: Waiter<T, TClose>[] = [];
+  let values: IteratorResult<T, TClose>[] = [];
   let didClose = false;
 
   let send = (value: T): void => {
@@ -30,7 +119,7 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
     }
   };
 
-  let close = (value: TReturn) => {
+  let close = (value: TClose) => {
     if(didClose) {
       new Error('tried to close an already closed queue');
     }
@@ -43,11 +132,11 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
     }
   };
 
-  let next = (): Operation<IteratorResult<T, TReturn>> => {
+  let next = (): Operation<IteratorResult<T, TClose>> => {
     return withLabels((task) => {
-      let { future, resolve } = createFuture<IteratorResult<T, TReturn>>();
+      let { future, resolve } = createFuture<IteratorResult<T, TClose>>();
       if(values.length) {
-        resolve(values.shift() as IteratorResult<T, TReturn>);
+        resolve(values.shift() as IteratorResult<T, TClose>);
       } else {
         waiters.push(resolve);
         task.consume(() => {
@@ -67,11 +156,11 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
 
   let subscription = {
     next,
-    close: ((...args) => close(args[0] as TReturn)) as Close<TReturn>,
+    close: ((...args) => close(args[0] as TClose)) as Close<TClose>,
     closeWith: close,
     first(): Operation<T | undefined> {
       return withName<T | undefined>(`first`, function*() {
-        let result: IteratorResult<T, TReturn> = yield next();
+        let result: IteratorResult<T, TClose> = yield next();
         if(result.done) {
           return undefined;
         } else {
@@ -82,7 +171,7 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
 
     expect(): Operation<T> {
       return withName('expect', function*() {
-        let result: IteratorResult<T, TReturn> = yield next();
+        let result: IteratorResult<T, TClose> = yield next();
         if(result.done) {
           throw new Error('expected to contain a value');
         } else {
@@ -91,10 +180,10 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
       });
     },
 
-    forEach(visit: (value: T) => (Operation<void> | void)): Operation<TReturn> {
+    forEach(visit: (value: T) => (Operation<void> | void)): Operation<TClose> {
       return withName('forEach', function* forEach() {
         while (true) {
-          let result: IteratorResult<T,TReturn> = yield next();
+          let result: IteratorResult<T,TClose> = yield next();
           if(result.done) {
             return result.value;
           } else {
@@ -107,12 +196,12 @@ export function createQueue<T, TReturn = undefined>(name = 'queue'): Queue<T, TR
       });
     },
 
-    join(): Operation<TReturn> {
+    join(): Operation<TClose> {
       return withName('join', subscription.forEach(() => { /* no op */ }));
     },
 
-    collect(): Operation<Iterator<T, TReturn>> {
-      return withName<Iterator<T, TReturn>>('collect', function*() {
+    collect(): Operation<Iterator<T, TClose>> {
+      return withName<Iterator<T, TClose>>('collect', function*() {
         let items: T[] = [];
         let result = yield subscription.forEach((item) => function*() { items.push(item) });
         return (function*() {

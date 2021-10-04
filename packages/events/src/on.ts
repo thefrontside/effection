c2cc518d487104b9b56d622ccd4113375622d8e6
Jonas Nicklas <jonas@jnicklas.com>
2021-10-04 14:23:40 +0200

    Improve events documentation

--- packages/events/src/on.ts
+++ packages/events/src/on.ts
@@ -4,12 +4,15 @@ import { EventSource, addListener, removeListener } from './event-source';
 
 
 /**
- * Creates a {@link Stream} from an event source and event name that
+ * Creates a `Stream` from an event source and event name that
  * produces the event as its next value every time that the source
  * raises it. Streams created in this way are infinite.
  *
- * Both {@link EventTarget} and {@link EventEmitter}
- * are supported.
+ * Both
+ * [`EventTarget`](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget)
+ * from the browser DOM and
+ * [`EventEmitter`](https://nodejs.org/api/events.html#events_class_eventemitter)
+ * from node are supported.
  *
  * ### Example
  *
@@ -30,6 +33,12 @@ import { EventSource, addListener, removeListener } from './event-source';
  * yield spawn(on(process.stdin, 'data').forEach(data => {
  *   buffer += data;
  * }));
+ * ```
+ *
+ * @param source an object which emits events
+ * @param name the name of the event to subscribe to
+ * @param streamName the name of the returned stream, useful for debugging
+ * @typeParam T the type of the argument to the emitted event
  */
 export function on<T = unknown>(source: EventSource, name: string, streamName = `on('${name}')`): Stream<T, void> {
   return createStream((publish) => withLabels(function*() {
@@ -44,10 +53,10 @@ export function on<T = unknown>(source: EventSource, name: string, streamName = 
 
 /**
  * Exactly like {@link on | on()} except each value produced by the
- * stream is an {@link Array} of all the arguments passed when the event was
+ * stream is an array of all the arguments passed when the event was
  * dispatched.
  *
- * This should rarely be needed, and never with {@link EventTarget}s
+ * This should rarely be needed, and never with `EventTarget`
  * which always invoke their event listeners with a single argument.
  *
  * ### Example
@@ -68,10 +77,14 @@ export function on<T = unknown>(source: EventSource, name: string, streamName = 
  * //=> 7 times 6 = 42!
  * ```
  *
+ * @param source an object which emits events
+ * @param name the name of the event to subscribe to
+ * @param streamName the name of the returned stream, useful for debugging
+ * @typeParam TArgs the type of the array of arguments to the emitted event
  */
-export function onEmit<T extends Array<unknown> = unknown[]>(source: EventSource, name: string, streamName = `onEmit('${name}')`): Stream<T, void> {
+export function onEmit<TArgs extends Array<unknown> = unknown[]>(source: EventSource, name: string, streamName = `onEmit('${name}')`): Stream<TArgs, void> {
   return createStream((publish) => withLabels(function*() {
-    let listener = (...args: T) => publish(args);
+    let listener = (...args: TArgs) => publish(args);
     addListener(source, name, listener);
     try {
       yield;

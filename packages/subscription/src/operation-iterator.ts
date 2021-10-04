0566c89c2254da126ab35086e37ebbb9cb3a15dc
Jonas Nicklas <jonas@jnicklas.com>
2021-10-04 14:59:10 +0200

    Add documentation for subscription

--- packages/subscription/src/operation-iterator.ts
+++ packages/subscription/src/operation-iterator.ts
@@ -1,5 +1,8 @@
 import { Operation } from '@effection/core';
 
+/**
+ * @hidden
+ */
 export interface OperationIterator<T, TReturn = undefined> {
   next(): Operation<IteratorResult<T, TReturn>>;
 }

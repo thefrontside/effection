62e54d27219f33bbf0f530e85f9f39e2494888fb
Charles Lowell <cowboyd@frontside.com>
2021-11-24 16:59:15 +0200

    format typescript docs below 80 chars

--- packages/core/src/abort-signal.ts
+++ packages/core/src/abort-signal.ts
@@ -3,17 +3,24 @@ import { Resource } from "./operation";
 import { AbortController, AbortSignal } from 'abort-controller';
 
 /**
- * Create an [`AbortSignal`](https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal) bound 
- * to the current scope. Whenever that scope is completed, errored, or halted, the abort signal will 
- * be triggered.
+ * Create an
+ * [`AbortSignal`](https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal)
+ * bound to the current scope. Whenever that scope is completed,
+ * errored, or halted, the abort signal will be triggered.
  *
- * Cancellation and teardown is handled handled automatically in Effection, but in systems where
- * it is not, the lifespan of a transaction needs to be explicitly modeled. A very common way to do this is with 
- * the `AbortSignal`. By creating an abort signal bound to an Effection task, we can pass that signal down to 
- * 3rd party consumers that can use it to register shutdown callbacks that will be invoked whenever the task is done
- * (either by completion, failure, or cancellation). An example is the native [`fetch()`](https://developer.mozilla.org/en-US/docs/Web/API/Fetch#syntax) API which takes a `signal` parameter in order to trigger when the HTTP request should
- * be cancelled.  This is how you would bind the lifetime of the HTTP request to the lifetime of the current task.
- * 
+ * Cancellation and teardown is handled automatically in Effection,
+ * but in systems where it is not, the lifespan of a transaction needs
+ * to be explicitly modeled. A very common way to do this is with the
+ * `AbortSignal`. By creating an abort signal bound to an Effection
+ * task, we can pass that signal down to 3rd party consumers that can
+ * use it to register shutdown callbacks that will be invoked whenever
+ * the task is done (either by completion, failure, or
+ * cancellation). An example is the native
+ * [`fetch()`](https://developer.mozilla.org/en-US/docs/Web/API/Fetch#syntax)
+ * API which takes a `signal` parameter in order to trigger when the
+ * HTTP request should be cancelled.  This is how you would bind the
+ * lifetime of the HTTP request to the lifetime of the current task.
+ *
  * ```js
  * function* request() {
  *   let signal = yield createAbortSignal();

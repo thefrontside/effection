6458c87e1e97443cb3ff88ea5e0d2a3758606530
Dmitry Lazarev <w@kich.dev>
2022-01-10 20:12:53 +0500

    add operation object and `toOperation` method

---
+++ packages/core/src/controller/object-controller.ts
@@ -0,0 +1,46 @@
+import { Task } from '../task';
+import { Controller } from './controller';
+import { createFuture } from '../future';
+import { OperationObject } from '../operation';
+
+export function createObjectController<TOut>(task: Task<TOut>, _obj: OperationObject<TOut>, createController: () => Controller<TOut>): Controller<TOut> {
+  let delegate: Controller<TOut>;
+  let { produce, future } = createFuture<TOut>();
+
+  function start() {
+    try {
+      delegate = createController();
+      task.setLabels({ ...delegate.operation?.labels });
+    } catch (error) {
+      produce({ state: 'errored', error });
+      return;
+    }
+    delegate.future.consume((value) => {
+      produce(value);
+    });
+    delegate.start();
+  }
+
+  function halt() {
+    delegate?.halt();
+  }
+
+  return {
+    get type() {
+      if(delegate) {
+        return delegate.type === 'promise' ? 'async function' : `${delegate.type} function`;
+      } else {
+        return 'function';
+      }
+    },
+    get operation() {
+      return delegate?.operation;
+    },
+    get resourceTask() {
+      return delegate?.resourceTask;
+    },
+    future,
+    start,
+    halt,
+  };
+}

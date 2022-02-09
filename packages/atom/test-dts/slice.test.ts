41f6786334313f8462bcb9c9ef3acaf504bcc641
Paul Cowan <paul.cowan@cutting.scot>
2022-02-09 16:15:42 +0000

    add deep test

--- packages/atom/test-dts/slice.test.ts
+++ packages/atom/test-dts/slice.test.ts
@@ -31,3 +31,54 @@ atom.slice('a').slice('c');
 
 atom.slice('a').slice('a').slice('b'); // $ExpectType Slice<string>
 atom.slice('a').slice('a', 'b'); // $ExpectType Slice<string>
+
+interface Deep {
+  a: {
+    b: {
+      c: {
+        d: {
+          e: {
+            f: {
+              g:{
+                h: {
+                  i: {
+                    j: {
+                      k: {
+                        l: {
+                          m: {
+                            n: {
+                              o: {
+                                p: {
+                                  q: {
+                                    r: {
+                                      s: {
+                                        t: {
+                                          u: 'u bet'
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+declare const deep: Slice<Deep>;
+
+deep.slice('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'); // $ExpectType Slice<string>
+
+// $ExpectError
+deep.slice('a', 'b', 'c', 'd', 'f');

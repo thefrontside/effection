a3a407ce01b00f2e5e5e6fec6e7ac4cfb9e5f17f
Jacob Bolda <me@jacobbolda.com>
2022-04-07 09:51:04 -0500

    undo formatting changes

--- packages/process/test/exec.test.ts
+++ packages/process/test/exec.test.ts
@@ -6,72 +6,60 @@ import { exec, Process, ProcessResult } from '../src';
 
 describe('exec', () => {
   describe('.join', () => {
-    it('runs successfully to completion', function* () {
-      let result: ProcessResult = yield exec(
-        "node './test/fixtures/hello-world.js'"
-      ).join();
+    it('runs successfully to completion', function*() {
+      let result: ProcessResult = yield exec("node './test/fixtures/hello-world.js'").join();
 
       expect(result.code).toEqual(0);
       expect(result.stdout).toEqual('hello\nworld\n');
       expect(result.stderr).toEqual('boom\n');
     });
 
-    it('runs failed process to completion', function* () {
-      let result: ProcessResult = yield exec(
-        "node './test/fixtures/hello-world-failed.js'"
-      ).join();
+    it('runs failed process to completion', function*() {
+      let result: ProcessResult = yield exec("node './test/fixtures/hello-world-failed.js'").join();
 
       expect(result.code).toEqual(37);
       expect(result.stdout).toEqual('hello world\n');
       expect(result.stderr).toEqual('boom\n');
     });
 
-    it('applies labels', function* () {
-      expect((exec('foo').join()?.labels as Labels)?.name).toEqual(
-        'exec("foo").join()'
-      );
+    it('applies labels', function*() {
+      expect((exec("foo").join()?.labels as Labels)?.name).toEqual('exec("foo").join()');
     });
   });
 
   describe('.expect', () => {
-    it('runs successfully to completion', function* () {
-      let result: ProcessResult = yield exec(
-        "node './test/fixtures/hello-world.js'"
-      ).expect();
+    it('runs successfully to completion', function*() {
+      let result: ProcessResult = yield exec("node './test/fixtures/hello-world.js'").expect();
 
       expect(result.code).toEqual(0);
       expect(result.stdout).toEqual('hello\nworld\n');
       expect(result.stderr).toEqual('boom\n');
     });
 
-    it('throws an error if process fails', function* () {
-      let error: Error = yield captureError(
-        exec("node './test/fixtures/hello-world-failed.js'").expect()
-      );
+    it('throws an error if process fails', function*() {
+      let error: Error = yield captureError(exec("node './test/fixtures/hello-world-failed.js'").expect());
 
       expect(error.name).toEqual('ExecError');
     });
 
-    it('applies labels', function* () {
-      expect((exec('foo').expect()?.labels as Labels)?.name).toEqual(
-        'exec("foo").expect()'
-      );
+    it('applies labels', function*() {
+      expect((exec("foo").expect()?.labels as Labels)?.name).toEqual('exec("foo").expect()');
     });
   });
 
-  it('applies labels to resource', function* () {
-    expect(exec('foo').name).toEqual('exec("foo")');
+  it('applies labels to resource', function*() {
+    expect(exec("foo").name).toEqual('exec("foo")');
   });
 
   describe('spawning', () => {
     describe('a process that fails to start', () => {
       describe('calling join()', () => {
-        it('reports the failed status', function* () {
+        it('reports the failed status', function*() {
           let error: unknown;
-          let proc = yield exec('argle', { arguments: ['bargle'] });
+          let proc = yield exec("argle", { arguments: ['bargle'] });
           try {
             yield proc.join();
-          } catch (e) {
+          } catch(e) {
             error = e;
           }
           expect(error).toBeInstanceOf(Error);
@@ -79,14 +67,12 @@ describe('exec', () => {
       });
 
       describe('calling expect()', () => {
-        it('fails', function* () {
+        it('fails', function*() {
           let error: unknown;
-          let proc = yield exec('argle', { arguments: ['bargle'] });
+          let proc = yield exec("argle", { arguments: ['bargle'] });
           try {
-            yield proc.expect();
-          } catch (e) {
-            error = e;
-          }
+            yield proc.expect()
+          } catch (e) { error = e; }
 
           expect(error).toBeDefined();
         });
@@ -98,7 +84,7 @@ describe('exec', () => {
       let joinStdout: Task;
       let joinStderr: Task;
 
-      beforeEach(function* () {
+      beforeEach(function*() {
         proc = yield exec("node './fixtures/echo-server.js'", {
           env: { PORT: '29000', PATH: process.env.PATH as string },
           cwd: __dirname,
@@ -107,36 +93,30 @@ describe('exec', () => {
         joinStdout = yield spawn(proc.stdout.join());
         joinStderr = yield spawn(proc.stderr.join());
 
-        yield proc.stdout
-          .lines()
-          .filter((v) => v.includes('listening'))
-          .expect();
+        yield proc.stdout.lines().filter((v) => v.includes('listening')).expect();
       });
 
-      it('has a pid', function* () {
+      it('has a pid', function*() {
         expect(typeof proc.pid).toBe('number');
         expect(proc.pid).not.toBeNaN();
       });
 
       describe('when it succeeds', () => {
-        beforeEach(function* () {
-          yield fetch('http://localhost:29000', {
-            method: 'POST',
-            body: 'exit',
-          });
+        beforeEach(function*() {
+          yield fetch('http://localhost:29000', { method: "POST", body: "exit" });
         });
 
-        it('joins successfully', function* () {
+        it('joins successfully', function*() {
           let status = yield proc.join();
           expect(status.code).toEqual(0);
         });
 
-        it('expects successfully', function* () {
+        it('expects successfully', function*() {
           let status = yield proc.expect();
           expect(status.code).toEqual(0);
         });
 
-        it('closes stdout and stderr', function* () {
+        it('closes stdout and stderr', function*() {
           yield proc.expect();
           expect(yield joinStdout).toEqual(undefined);
           expect(yield joinStderr).toEqual(undefined);
@@ -144,20 +124,17 @@ describe('exec', () => {
       });
 
       describe('when it fails', () => {
-        let error: Error;
-        beforeEach(function* () {
-          yield fetch('http://localhost:29000', {
-            method: 'POST',
-            body: 'fail',
-          });
+        let error: Error
+        beforeEach(function*() {
+          yield fetch('http://localhost:29000', { method: "POST", body: "fail" });
         });
 
-        it('joins successfully', function* () {
+        it('joins successfully', function*() {
           let status = yield proc.join();
           expect(status.code).not.toEqual(0);
         });
 
-        it('expects unsuccessfully', function* () {
+        it('expects unsuccessfully', function*() {
           yield function* () {
             try {
               yield proc.expect();
@@ -165,10 +142,10 @@ describe('exec', () => {
               error = e;
             }
           };
-          expect(error).toBeDefined();
+          expect(error).toBeDefined()
         });
 
-        it('closes stdout and stderr', function* () {
+        it('closes stdout and stderr', function*() {
           expect(yield joinStdout).toEqual(undefined);
           expect(yield joinStderr).toEqual(undefined);
         });
@@ -181,26 +158,26 @@ describe('exec', () => {
   // in windows at all.
   if (process.platform !== 'win32') {
     describe('when the `shell` option is true', () => {
-      it('lets the shell do all of the shellword parsing', function* () {
+      it('lets the shell do all of the shellword parsing', function*() {
         let proc = exec('echo "first" | echo "second"', {
-          shell: true,
+          shell: true
         });
         let { stdout }: ProcessResult = yield proc.expect();
 
-        expect(stdout).toEqual('second\n');
-      });
+        expect(stdout).toEqual("second\n");
+      })
     });
   }
 
   describe('when the `shell` option is `false`', () => {
-    it('automatically parses the command arguments using shellwords', function* () {
+    it('automatically parses the command argumens using shellwords', function*() {
       let proc = exec('echo "first" | echo "second"', {
-        shell: false,
+        shell: false
       });
       let { stdout }: ProcessResult = yield proc.expect();
 
-      expect(stdout).toEqual('first | echo second\n');
-    });
+      expect(stdout).toEqual("first | echo second\n");
+    })
   });
 
   describe('handles env vars', () => {

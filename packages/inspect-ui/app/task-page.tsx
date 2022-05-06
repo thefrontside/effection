b2c5097ab4bc64e9ef80eee019d7cc14d4b58edd
Taras <tarasm@gmail.com>
2022-05-06 12:50:20 -0400

    Upgraded to react-router@6.3.0

---
+++ packages/inspect-ui/app/task-page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useParams, Link, Navigate } from "react-router-dom";
+import { InspectStateSlice } from "./app";
+import { TaskTreeRoot } from "./task-tree-root";
+
+export function TaskPage({ slice }: { slice: InspectStateSlice }): JSX.Element {
+  let { id } = useParams();
+
+  let targetSlice = findSliceById(slice, Number(id));
+  if (targetSlice) {
+    return (
+      <div>
+        <p>
+          <Link to="/" className="inspector--main--return">
+            ← Show all
+          </Link>
+        </p>
+
+        <TaskTreeRoot slice={targetSlice} />
+      </div>
+    );
+  } else {
+    return <Navigate to="/" />;
+  }
+}
+
+function findSliceById(
+  slice: InspectStateSlice,
+  targetId: number
+): InspectStateSlice | undefined {
+  let task = slice.get();
+
+  if (task.id === targetId) {
+    return slice;
+  }
+
+  if (task.yieldingTo) {
+    let result = findSliceById(
+      slice.slice("yieldingTo") as InspectStateSlice,
+      targetId
+    );
+    if (result) {
+      return result;
+    }
+  }
+
+  for (let [index] of task.children.entries()) {
+    let result = findSliceById(slice.slice("children", index), targetId);
+    if (result) {
+      return result;
+    }
+  }
+
+  return undefined;
+}

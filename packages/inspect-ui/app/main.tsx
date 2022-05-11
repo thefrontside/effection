cdcda2ef5102ddcbf015ff888dc4cb50c1c02198
Taras <tarasm@gmail.com>
2022-05-11 15:27:33 -0400

    Add theme toggle option

--- packages/inspect-ui/app/main.tsx
+++ packages/inspect-ui/app/main.tsx
@@ -1,20 +1,30 @@
-import { main } from 'effection';
-import { HashRouter } from 'react-router-dom';
-import { Slice } from '@effection/atom';
-import { createWebSocketClient, WebSocketClient } from '@effection/websocket-client';
-import { EffectionContext } from '@effection/react';
-import { ClientMessage, InspectState, inspectState } from '@effection/inspect-utils';
+import { main } from "effection";
+import { HashRouter } from "react-router-dom";
+import { Slice } from "@effection/atom";
+import {
+  createWebSocketClient,
+  WebSocketClient,
+} from "@effection/websocket-client";
+import { EffectionContext } from "@effection/react";
+import {
+  ClientMessage,
+  InspectState,
+  inspectState,
+} from "@effection/inspect-utils";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { App } from './app';
+import { App } from "./app";
+import { Theme } from "./theme";
 
-main(function*(scope) {
+main(function* (scope) {
   let url = new URL(location.href);
-  let port = url.searchParams.get('port') || location.port;
+  let port = url.searchParams.get("port") || location.port;
 
-  let client: WebSocketClient<ClientMessage> = yield createWebSocketClient(`ws://localhost:${port}`);
+  let client: WebSocketClient<ClientMessage> = yield createWebSocketClient(
+    `ws://localhost:${port}`
+  );
   console.log("Client connected");
 
   let slice: Slice<InspectState> = yield inspectState(client);
@@ -22,10 +32,12 @@ main(function*(scope) {
   ReactDOM.render(
     <EffectionContext.Provider value={scope}>
       <HashRouter>
-        <App slice={slice}/>
+        <Theme>
+          <App slice={slice} />
+        </Theme>
       </HashRouter>
     </EffectionContext.Provider>,
-    document.querySelector('#app')
+    document.querySelector("#app")
   );
   yield;
 });

41dcfb61f785f1b1c8d12724de229a30c6e34260
Jacob Bolda <me@jacobbolda.com>
2022-08-16 08:40:46 -0500

    implement @effection/vitest package

---
+++ packages/vitest/src/index.ts
@@ -0,0 +1,192 @@
+import {
+  Effection,
+  Resource,
+  Task,
+  sleep,
+  Operation,
+  race,
+  run,
+} from 'effection';
+import * as vitestGlobals from 'vitest';
+import type { SuiteHooks, TestContext, Suite, File } from 'vitest';
+import { assert } from 'assert-ts';
+
+export interface TestFn {
+  (this: TestContext, world: Task, scope: Task): ReturnType<
+    Resource<void>['init']
+  >;
+}
+
+export interface ItFn {
+  (title: string, fn: TestFn, timeout?: number): void;
+}
+
+export interface It extends ItFn {
+  only: ItFn;
+  skip: ItFn;
+  eventually: ItFn;
+  todo(title: string): void;
+}
+
+const scopes = {} as {
+  all: Task | undefined;
+  each: Task | undefined;
+};
+
+async function withinScope(
+  name: keyof typeof scopes,
+  fn: (task: Task) => Promise<void>
+) {
+  let scope = scopes[name];
+  assert(!!scope, `critical: test scope '${name}' was not initialized`);
+  if (scope.state === 'errored' || scope.state === 'erroring') {
+    await scope;
+  }
+  await fn(scope);
+  if (scope.state === 'errored' || scope.state === 'erroring') {
+    await scope;
+  }
+}
+
+function runInAllScope(fn: Suite | File, name: string) {
+  return async function (this: TestContext) {
+    await withinScope('all', async (all) => {
+      await all
+        .run({
+          name,
+          init: fn.bind(this ?? {}),
+        })
+        .catchHalt();
+    });
+  };
+}
+
+function runInEachScope(fn: TestContext, name: string) {
+  return async function (this: TestContext) {
+    await withinScope('all', async () => {
+      await withinScope('each', async (each) => {
+        await each
+          .run({
+            name,
+            init: fn.bind(this ?? {}),
+          })
+          .catchHalt();
+      });
+    });
+  };
+}
+
+vitestGlobals.beforeAll(async () => {
+  await Effection.reset();
+  scopes.all = run(undefined, { labels: { name: 'allScope' } });
+});
+
+vitestGlobals.afterAll(async () => {
+  scopes.all = undefined;
+  await Effection.halt();
+});
+
+vitestGlobals.beforeEach(async () => {
+  scopes.each = run(undefined, { labels: { name: 'eachScope' } });
+});
+
+vitestGlobals.afterEach(async () => {
+  let each = scopes.each;
+  if (!!each) {
+    each.halt();
+    await each.catchHalt();
+    scopes.each = undefined;
+  }
+});
+
+export function beforeAll(
+  fn: SuiteHooks['beforeAll'][0],
+  timeout?: number
+): void {
+  return vitestGlobals.beforeAll(runInAllScope(fn, 'beforeAll'), timeout);
+}
+
+export function beforeEach(
+  fn: SuiteHooks['beforeEach'][0],
+  timeout?: number
+): void {
+  return vitestGlobals.beforeEach(runInEachScope(fn, 'beforeEach'), timeout);
+}
+
+export const describe = vitestGlobals.describe;
+
+export const it: It = Object.assign(
+  function it(name: string, fn: TestFn, timeout?: number) {
+    return vitestGlobals.it(
+      name,
+      runInEachScope(fn, `it(${JSON.stringify(name)})`),
+      timeout
+    );
+  },
+  {
+    only(name: string, fn: TestFn, timeout?: number): void {
+      return vitestGlobals.it.only(
+        name,
+        runInEachScope(fn, `it(${JSON.stringify(name)})`),
+        timeout
+      );
+    },
+    skip(name: string, fn: TestFn, timeout?: number): void {
+      return vitestGlobals.it.skip(
+        name,
+        runInEachScope(fn, `it(${JSON.stringify(name)})`),
+        timeout
+      );
+    },
+    todo(name: string): void {
+      return vitestGlobals.it.todo(name);
+    },
+    eventually(name: string, fn: TestFn, testTimeout?: number) {
+      let limit = testTimeout ?? 10000; //getState().testTimeout; TODO
+
+      return it(
+        name,
+        function* (scope, current) {
+          let operation = fn.bind(this) as (
+            scope: Task,
+            current: Task
+          ) => Operation<void>;
+          let error = new Error(
+            `operation never succeeded within the ${limit}ms limit`
+          );
+          function* trial(): Operation<void> {
+            while (true) {
+              try {
+                yield operation(scope, current);
+                break;
+              } catch (e) {
+                error = e as Error;
+                yield sleep(1);
+              }
+            }
+          }
+          function* timeout() {
+            yield sleep(limit);
+            throw error;
+          }
+          yield race([trial, timeout]);
+        },
+        limit * 3
+      );
+    },
+  }
+);
+export const test = it;
+
+export function captureError(
+  op: Operation<unknown>
+): Operation<unknown | Error> {
+  return function* () {
+    try {
+      yield op;
+    } catch (error) {
+      return error;
+    }
+    throw new Error('expected operation to throw an error, but it did not!');
+  };
+}
---
+++ packages/vitest/test/vitest.test.ts
@@ -0,0 +1,157 @@
+import { describe, it, beforeEach, captureError } from '../src/index';
+import { expect } from 'vitest';
+import { Task, Resource, spawn, sleep } from 'effection';
+import { exec } from '@effection/process';
+import { dirname } from 'path';
+
+let captured: Task;
+
+function* boom() {
+  throw new Error('boom');
+}
+
+const myResource: Resource<Task> = {
+  *init() {
+    return yield spawn();
+  },
+};
+
+const cwd = dirname(__dirname);
+
+describe('@effection/vitest', () => {
+  it('applies labels', function* (scope) {
+    expect(scope.labels.name).toContain('it("applies labels")');
+  });
+
+  it(
+    'throws error on failure',
+    function* () {
+      let { code, stderr } = yield exec(
+        'yarn vitest run direct-error.failure.ts --no-color --config vite.test.config.ts',
+        { cwd }
+      ).join();
+      expect(code).toEqual(1);
+      expect(stderr).toContain('boom\n');
+    },
+    process.env.CI ? 30000 : undefined
+  );
+
+  it(
+    'throws error on failure in background task',
+    function* () {
+      let { code, stderr } = yield exec(
+        'yarn vitest run spawned-error.failure.ts --no-color --config vite.test.config.ts',
+        { cwd }
+      ).join();
+      expect(code).toEqual(1);
+      expect(stderr).toContain('boom');
+    },
+    process.env.CI ? 30000 : undefined
+  );
+
+  it(
+    'throws error on failure in suite scope',
+    function* () {
+      let { code, stderr } = yield exec(
+        'yarn vitest run error-in-beforeall.failure.ts --no-color --config vite.test.config.ts',
+        { cwd }
+      ).join();
+      expect(code).toEqual(1);
+      expect(stderr).toContain('boom');
+    },
+    process.env.CI ? 30000 : undefined
+  );
+
+  it(
+    'throws error on failure in suite scope',
+    function* () {
+      let { code, stderr } = yield exec(
+        'yarn vitest run beforeach-spawned-failure.ts --no-color --config vite.test.config.ts',
+        { cwd }
+      ).join();
+      expect(code).toEqual(1);
+      expect(stderr).toContain('boom');
+    },
+    process.env.CI ? 30000 : undefined
+  );
+
+  it.todo('can have pending tasks (note: this is not actually pending)');
+
+  describe('accessing the Vitest Context API', () => {
+    beforeEach(function* () {
+      this.contextValue = 'hello Vitest';
+    });
+
+    it('works', function* () {
+      expect(this.contextValue).toEqual('hello Vitest');
+    });
+  });
+
+  describe('cleaning up tasks', () => {
+    it('sets up task', function* (task) {
+      captured = yield task.spawn();
+    });
+
+    it('and cleans it up', function* () {
+      expect(captured.state).toEqual('halted');
+    });
+  });
+
+  describe('captureError', () => {
+    it('returns error thrown by given operation', function* () {
+      expect(yield captureError(boom)).toHaveProperty('message', 'boom');
+    });
+
+    it('throws an error if given operation does not throw an error', function* () {
+      expect(yield captureError(captureError(function* () {}))).toHaveProperty(
+        'message',
+        'expected operation to throw an error, but it did not!'
+      );
+    });
+  });
+
+  describe('spawning in world', () => {
+    beforeEach(function* (world) {
+      captured = yield world.spawn();
+    });
+
+    it('does not halt the spawned task before it block', function* () {
+      expect(captured.state).toEqual('running');
+    });
+  });
+
+  describe('spawning in scope', () => {
+    beforeEach(function* (_world, scope) {
+      captured = yield scope.spawn();
+    });
+
+    it('halts the spawned task before it block', function* () {
+      expect(captured.state).toEqual('halted');
+    });
+  });
+
+  describe('spawning resource in world', () => {
+    beforeEach(function* () {
+      captured = yield myResource;
+    });
+
+    it('keeps running beyond the before each block', function* () {
+      expect(captured.state).toEqual('running');
+    });
+  });
+
+  describe('.eventually()', () => {
+    beforeEach(function* () {
+      this.tries = 0;
+    });
+
+    it.eventually(
+      'passes if the operation passes within timeout',
+      function* () {
+        yield sleep(1);
+        (this.tries as number)++;
+        expect(this.tries).toBeGreaterThan(10);
+      }
+    );
+  });
+});

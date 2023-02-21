85f5b721539cb7909781ebc64eed19a9ac52ddb3
Charles Lowell <cowboyd@frontside.com>
2023-02-21 14:48:13 -0600

    Upgrade to latest instructional effection

--- mod.ts
+++ mod.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/cowboyd/instructional-effection/v0.0.0/mod.ts";
+export * from "https://raw.githubusercontent.com/cowboyd/instructional-effection/v0.0.1/mod.ts";

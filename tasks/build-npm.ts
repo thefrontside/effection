4932ed28ab919e9698d4f4dd28874abbcc414e1b
Charles Lowell <cowboyd@frontside.com>
2023-03-21 12:19:04 -0500

    release effection v3 alpha to npm

---
+++ tasks/build-npm.ts
@@ -0,0 +1,45 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
+
+const outDir = "./build/npm";
+
+await emptyDir(outDir);
+
+let [version] = Deno.args;
+if (!version) {
+  throw new Error("a version argument is required to build the npm package");
+}
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir,
+  shims: {
+    deno: false,
+  },
+  test: false,
+  typeCheck: false,
+  compilerOptions: {
+    lib: ["esnext"],
+    target: "ES2020",
+    sourceMap: true,
+  },
+  package: {
+    // package.json properties
+    name: "effection",
+    version,
+    description: "Structured concurrency and effects for JavaScript",
+    license: "ISC",
+    repository: {
+      author: "engineering@frontside.com",
+      type: "git",
+      url: "git+https://github.com/thefrontside/effection.git",
+    },
+    bugs: {
+      url: "https://github.com/thefrontside/effection/issues",
+    },
+    engines: {
+      node: ">= 16",
+    },
+  },
+});
+
+await Deno.copyFile("README.md", `${outDir}/README.md`);

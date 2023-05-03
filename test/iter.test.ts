44ca5ca501882312a4809608078ff2fc9da70942
Charles Lowell <cowboyd@frontside.com>
2023-05-03 14:11:50 -0500

    implement the outer iterator directly with `next()`

--- test/iter.test.ts
+++ test/iter.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "./suite.ts";
+import { createFuture } from "../lib/future.ts";
 import { createChannel, Operation, run, sleep, spawn, Stream } from "../mod.ts";
 
 let tests = describe("iter()");
@@ -11,59 +12,72 @@ function* iterate<T, TResult>(
   stream: Stream<T, TResult>,
 ): Operation<Iteration<T, TResult>> {
   let sub = yield* stream;
-  let first = yield* sub;
-  let result: TResult;
-  if (first.done) {
-    result = first.value;
+  let latest = yield* sub;
+
+  let { future: result, resolve } = createFuture<TResult>();
+
+  function next(): IteratorResult<Operation<T>, TResult> {
+    if (latest.done) {
+      return latest;
+    } else {
+      return {
+        done: false,
+        value: {
+          *[Symbol.iterator]() {
+            let { value } = latest;
+            latest = yield* sub;
+            if (latest.done) {
+              resolve(latest.value);
+            }
+            // we know that this must be a `T`, not `TResult` because
+            // it is left over from the last iteration which we we checked
+            // on the last iteration on line 29.
+            return value as T;
+          },
+        },
+      };
+    }
   }
 
   return {
-    *[Symbol.iterator]() {
-      if (result) {
-        return result;
-      }
-
-      return {
-        *[Symbol.iterator]() {
-          yield (first.value) as T;
-          let next = first;
-
-          while (true) {
-            // next = yield* sub;
-            yield next;
-          }
-        },
-      };
-    },
-    result: {
-      *[Symbol.iterator]() {
-        return result;
-      },
-    },
+    [Symbol.iterator]: () => ({ next }),
+    result,
   };
 }
 
 it(tests, "should iterate", async () => {
-  const chan = createChannel<string>();
+  const chan = createChannel<string, number>();
   let actual = await run(function* () {
     yield* spawn(function* () {
       yield* sleep(10);
       yield* chan.input.send("1");
       yield* chan.input.send("2");
       yield* chan.input.send("3");
-      yield* chan.input.close();
+      yield* chan.input.close(42);
     });
 
     let iter = yield* iterate(chan.output);
     let arr: string[] = [];
-    for (let msg of yield* iter) {
-      console.log(msg);
+    for (let msg of iter) {
       let val = yield* msg;
-      console.log(val);
       arr.push(val);
     }
-    return { arr, result: iter.result};
+    return { arr, result: yield* iter.result };
   });
 
-  expect(actual).toEqual({ arr: [], result: undefined });
+  expect(actual).toEqual({ arr: ["1", "2", "3"], result: 42 });
+});
+
+it(tests, "should iterate an empty stream", async () => {
+  const chan = createChannel<string, number>();
+  await run(function* () {
+    yield* spawn(function* () {
+      yield* sleep(10);
+      yield* chan.input.close(42);
+    });
+
+    for (let msg of yield* iterate(chan.output)) {
+      throw new Error("This block of code should never be reached: " + msg);
+    }
+  });
 });

2e79959aa5b9de20d9fa9699f97ac945851616d2
Paul Cowan <paul.cowan@cutting.scot>
2023-05-08 10:49:05 +0100

    allow EventEmitter as target

--- lib/events.ts
+++ lib/events.ts
@@ -1,8 +1,10 @@
 // deno-lint-ignore-file no-explicit-any ban-types
 import { createChannel } from "./channel.ts";
-import { action, resource, suspend } from "./instructions.ts";
+import { resource } from "./instructions.ts";
+import { first } from "./mod.ts";
 import { useScope } from "./run/scope.ts";
 import type { Operation, Stream } from "./types.ts";
+import { EventEmitter } from "node:events";
 
 type FN = (...any: any[]) => any;
 
@@ -17,91 +19,38 @@ export type EventList<T> = T extends {
 } ? P & string
   : never;
 
-/**
- * Takes an event source and event name and returns an
- * {@link effection:Operation} that produces the value emitted by that
- * event. Both {@link EventTarget} and {@link EventEmitter} are
- * supported.
- *
- * ### Example
- *
- * ``` javascript
- * let start = yield once(document, 'dragstart');
- * console.log(`drag started at (${start.pageX}, ${start.pageY})`);
- *
- * let end = yield once(document, 'dragend');
- * console.log(`drag ended at (${end.pageX}, ${end.pageY})`);
- * ```
- *
- * ### Example
- *
- * ``` javascript
- * let src = yield once(stream, 'pipe');
- * ```
- *
- * @param source an object which emits events
- * @param eventName the name of the event to subscribe to
- * @typeParam T the type of the argument to the emitted event
- */
 export function once<
-  T extends EventTarget,
+  T extends EventTarget | EventEmitter,
   K extends EventList<T> | (string & {}),
 >(target: T, name: K): Operation<EventTypeFromEventTarget<T, K>> {
-  return action(function* (resolve) {
-    target.addEventListener(
-      name,
-      resolve as EventListenerOrEventListenerObject,
-    );
+  return resource(function* (provide) {
+    let { input, output } = createChannel<Event, never>();
+    let scope = yield* useScope();
+
+    let listener = (event: Event) => scope.run(() => input.send(event));
+
+    if ("addEventListener" in target) {
+      target.addEventListener(name, listener);
+    } else if ("addListener" in target) {
+      target.addListener(name, listener);
+    }
+
     try {
-      yield* suspend();
-    } finally {
-      target.removeEventListener(
-        name,
-        resolve as EventListenerOrEventListenerObject,
+      yield* provide(
+        yield* first(output as Stream<EventTypeFromEventTarget<T, K>, never>),
       );
+    } finally {
+      if ("removeEventListener" in target) {
+        target.removeEventListener(name, listener);
+      } else {
+        target.removeListener(name, listener);
+      }
     }
   });
 }
 
-/**
- * Creates a `Stream` from an event source and event name that
- * produces the event as its next value every time that the source
- * raises it. Streams created in this way are infinite.
- *
- * Both
- * [`EventTarget`](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget)
- * from the browser DOM and
- * [`EventEmitter`](https://nodejs.org/api/events.html#events_class_eventemitter)
- * from node are supported.
- *
- * ### Example
- *
- * log every click in a web page:
- *
- * ```javascript
- * yield spawn(on(document, 'click').forEach(event => {
- *   console.log(`click at (${event.pageX}, ${event.pageY})`);
- * }));
- * ```
- *
- * ### Example
- *
- * buffer a node process's stdin into a single string:
- *
- * ```javascript
- * let buffer = '';
- * yield spawn(on(process.stdin, 'data').forEach(data => {
- *   buffer += data;
- * }));
- * ```
- *
- * @param source an object which emits events
- * @param name the name of the event to subscribe to
- * @param streamName the name of the returned stream, useful for debugging
- * @typeParam T the type of the argument to the emitted event
- */
 export function on<
-  T extends EventTarget,
+  T extends EventTarget | EventEmitter,
   K extends EventList<T> | (string & {}),
 >(target: T, name: K): Stream<EventTypeFromEventTarget<T, K>, never> {
   return resource(function* (provide) {
@@ -109,14 +58,22 @@ export function on<
     let scope = yield* useScope();
     let listener = (event: Event) => scope.run(() => input.send(event));
 
-    target.addEventListener(name, listener);
+    if ("addEventListener" in target) {
+      target.addEventListener(name, listener);
+    } else if ("addListener" in target) {
+      target.addListener(name, listener);
+    }
 
     try {
       yield* provide(
         yield* output as Stream<EventTypeFromEventTarget<T, K>, never>,
       );
     } finally {
-      target.removeEventListener(name, listener);
+      if ("removeEventListener" in target) {
+        target.removeEventListener(name, listener);
+      } else {
+        target.removeListener(name, listener);
+      }
     }
   });
 }
--- test/events.test.ts
+++ test/events.test.ts
@@ -1,22 +1,56 @@
-import { on, once } from "../mod.ts";
+import { on, once, run } from "../mod.ts";
 import type { Operation, Stream } from "../mod.ts";
-import { describe, expectType, it } from "./suite.ts";
+import { describe, expect, expectType, it } from "./suite.ts";
+import { EventEmitter } from "node:events";
 
 describe("events", () => {
-  const domElement = {} as HTMLElement;
-  let socket = {} as WebSocket;
+  describe("eventEmitter", () => {
+    it("on", () =>
+      run(function* () {
+        let eventEmitter = new EventEmitter();
+        let subscription = yield* on(eventEmitter, "event");
 
-  it("should find event from eventTarget", () => {
-    expectType<Operation<CloseEvent>>(once(socket, "close"));
-    expectType<Operation<MouseEvent>>(once(domElement, "click"));
+        eventEmitter.emit("event", 1);
 
-    // deno-lint-ignore no-explicit-any
-    expectType<Stream<MessageEvent<any>, never>>(on(socket, "message"));
+        let next = yield* subscription.next();
+
+        expect(next).toEqual({ done: false, value: 1 });
+      }));
+
+    it("once", () =>
+      run(function* () {
+        let eventEmitter = new EventEmitter();
+        let subscription = yield* on(eventEmitter, "event");
+
+        eventEmitter.emit("event", 1);
+
+        let next = yield* subscription.next();
+
+        expect(next).toEqual({ done: false, value: 1 });
+      }));
   });
 
-  it("should fall back to event", () => {
-    expectType<Operation<Event>>(once(domElement, "mycustomevent"));
+  describe("types", () => {
+    const domElement = {} as HTMLElement;
+    const socket = {} as WebSocket;
 
-    expectType<Stream<Event, never>>(on(domElement, "anothercustomevent"));
+    it("should find event from eventTarget", () => {
+      expectType<Operation<CloseEvent>>(once(socket, "close"));
+      expectType<Operation<MouseEvent>>(once(domElement, "click"));
+
+      // deno-lint-ignore no-explicit-any
+      expectType<Stream<MessageEvent<any>, never>>(on(socket, "message"));
+    });
+
+    it("should fall back to event", () => {
+      expectType<Operation<Event>>(once(domElement, "mycustomevent"));
+
+      expectType<Stream<Event, never>>(on(domElement, "anothercustomevent"));
+    });
+
+    it("should type eventEmitter", () => {
+      let eventEmitter = new EventEmitter();
+      expectType<Stream<Event, never>>(on(eventEmitter, "event"));
+    });
   });
 });

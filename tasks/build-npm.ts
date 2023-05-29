6295ed4f3e9fa440c60ccf9941919b69324215da
Eric Bower <me@erock.io>
2023-05-29 14:38:43 -0400

    pin esm.sh deps

--- tasks/build-npm.ts
+++ tasks/build-npm.ts
@@ -1,4 +1,4 @@
-import { build, emptyDir } from "https://deno.land/x/dnt@0.36.0/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.36.0/mod.ts?pin=v123";
 
 const outDir = "./build/npm";
 

691638c2e21f17ef09154734045f89372dd0b43d
Charles Lowell <cowboyd@frontside.com>
2023-07-06 18:16:44 +0300

    ✨Add all()
    The docs use `all()` and `race()` extensively, so we need to have at
    least some plausible versions of these functions.
    
    This ports over the tests from `v2`, and is implemented as about the
    simplest version of `all()` that I could get working. However, there
    is a failing test, and while I think that we can ship it while it
    still fails, we do need to fix it before releasing `3.0.0`.
    
    It fails because we need to catch an error in a resource while it is
    initializing. We could have any number of resources running, and if
    any of them fails before the all is completed, we need to be able to
    catch it.

---
+++ test/all.test.ts
@@ -0,0 +1,95 @@
+import {
+  asyncReject,
+  asyncResolve,
+  asyncResource,
+  describe,
+  expect,
+  it,
+  syncReject,
+  syncResolve,
+} from "./suite.ts";
+
+import { all, run, sleep } from "../mod.ts";
+
+describe("all()", () => {
+  it("resolves when the given list is empty", async () => {
+    let result = await run(() => all([]));
+
+    expect(result).toEqual([]);
+  });
+
+  it("resolves when all of the given operations resolve", async () => {
+    let result = await run(() =>
+      all([
+        syncResolve("quox"),
+        asyncResolve(10, "foo"),
+        asyncResolve(5, "bar"),
+        asyncResolve(15, "baz"),
+      ])
+    );
+
+    expect(result).toEqual(["quox", "foo", "bar", "baz"]);
+  });
+
+  it("rejects when one of the given operations rejects asynchronously first", async () => {
+    let result = run(() =>
+      all([
+        asyncResolve(10, "foo"),
+        asyncReject(5, "bar"),
+        asyncResolve(15, "baz"),
+      ])
+    );
+
+    await expect(result).rejects.toHaveProperty("message", "boom: bar");
+  });
+
+  it("rejects when one of the given operations rejects asynchronously and another operation does not complete", async () => {
+    let result = run(() => all([sleep(0), asyncReject(5, "bar")]));
+
+    await expect(result).rejects.toHaveProperty("message", "boom: bar");
+  });
+
+  it("resolves when all of the given operations resolve synchronously", async () => {
+    let result = run(() =>
+      all([
+        syncResolve("foo"),
+        syncResolve("bar"),
+        syncResolve("baz"),
+      ])
+    );
+
+    await expect(result).resolves.toEqual(["foo", "bar", "baz"]);
+  });
+
+  it("rejects when one of the given operations rejects synchronously first", async () => {
+    let result = run(() =>
+      all([
+        syncResolve("foo"),
+        syncReject("bar"),
+        syncResolve("baz"),
+      ])
+    );
+
+    await expect(result).rejects.toHaveProperty("message", "boom: bar");
+  });
+
+  it("rejects when one of the operations reject", async () => {
+    await run(function* () {
+      let fooStatus = { status: "pending" };
+      let error;
+      try {
+        yield* all([
+          asyncResource(20, "foo", fooStatus),
+          asyncReject(10, "bar"),
+        ]);
+      } catch (err) {
+        error = err;
+      }
+
+      expect(fooStatus.status).toEqual("pending");
+      expect(error).toHaveProperty("message", "boom: bar");
+      yield* sleep(40);
+      expect(fooStatus.status).toEqual("pending");
+    });
+  });
+});

4fded531d53a0b23cb8379544643e6fa7af88e26
Charles Lowell <cowboyd@frontside.com>
2023-07-25 15:06:17 +0300

    Add three column responsive navigation

--- www/docs/docs.ts
+++ www/docs/docs.ts
@@ -5,12 +5,6 @@ import remarkFrontmatter from "https://esm.sh/remark-frontmatter@4.0.1";
 import remarkMdxFrontmatter from "https://esm.sh/remark-mdx-frontmatter@3.0.0";
 import remarkGfm from "https://esm.sh/remark-gfm@3.0.1";
 
-import rehypePrismPlus from "https://esm.sh/rehype-prism-plus@1.5.1";
-import rehypeAutolinkHeadings from "https://esm.sh/rehype-autolink-headings@6.1.1";
-import rehypeAddClasses from "https://esm.sh/rehype-add-classes@1.0.0";
-import rehypeSlug from "https://esm.sh/rehype-slug@5.1.0";
-import rehypeToc from "https://esm.sh/@jsdevtools/rehype-toc@3.0.2";
-
 import { evaluate } from "https://esm.sh/@mdx-js/mdx@2.3.0";
 
 import { Fragment, jsx, jsxs } from "hastx/jsx-runtime";
@@ -67,27 +61,6 @@ export function* loadDocs(): Operation<Docs> {
           remarkFrontmatter,
           remarkMdxFrontmatter,
           remarkGfm,
-        ],
-        rehypePlugins: [
-          rehypeSlug,
-          [rehypeAutolinkHeadings, {
-            behavior: "append",
-            properties: {
-              className:
-                "opacity-0 group-hover:opacity-100 after:content-['#']",
-            },
-          }],
-          [rehypeAddClasses, {
-            "h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]": "group",
-          }],
-          [rehypeToc, {
-            cssClasses: {
-              toc: "fixed top-0 right-0",
-            },
-          }],
-          [rehypePrismPlus, {
-            showLineNumbers: true,
-          }],
         ],
       }));
 

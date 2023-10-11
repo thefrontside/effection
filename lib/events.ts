0c405e2911c0a3ec0f8293ccee5cddd2d4857359
Charles Lowell <cowboyd@frontside.com>
2023-10-11 16:02:33 -0500

    Add typedocs for `on()` and `once()`

--- lib/events.ts
+++ lib/events.ts
@@ -20,6 +20,14 @@ export type EventList<T> = T extends {
 } ? P & string
   : never;
 
+/**
+ * Create an {@link Operation} that yields the next event to be emitted by an
+ * [EventTarget](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget).
+ *
+ * @param target - the event target to be watched
+ * @param name - the name of the event to watch. E.g. "click"
+ * @returns an Operation that yields the next emitted event
+ */
 export function once<
   T extends EventTarget,
   K extends EventList<T> | (string & {}),
@@ -27,6 +35,17 @@ export function once<
   return first(on(target, name));
 }
 
+/**
+ * Create a {@link Stream} of events from any
+ * [EventTarget](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget).
+ *
+ * See the guide on [Streams and Subscriptions](https://frontside.com/effection/docs/collections)
+ * for details on how to use streams.
+ *
+ * @param target - the event target whose events will be streamed
+ * @param name - the name of the event to stream. E.g. "click"
+ * @returns a stream that will see one item for each event
+ */
 export function on<
   T extends EventTarget,
   K extends EventList<T> | (string & {}),

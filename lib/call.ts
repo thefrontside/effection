dfe3276d041d2c56451fc023dcea42a601c05400
Charles Lowell <cowboyd@frontside.com>
2023-10-27 17:52:14 -0500

    ✨Beef up the `call()` function to promises and async fns
    Right now there are a lot of different ways to convert values into
    operations. There is `expect()` (Promise->Operation),
    `call()(Operation->Operation)`, and `op` (Function -> Operation>.
    
    starfx [unifies all of these concepts into a single
    operation](https://github.com/neurosnap/starfx/blob/main/fx/call.ts)
    
    We should bring this into Effection core.
    
    This adds the following variations
    
    ```ts
    // promise
    call(Promise.resolve("hello"));
    call(async () => "hello");
    
    // Operation
    call({ *[Symbol.iterator]() { return "hello" } } );
    call(function*() { return "hello" });
    ```

--- lib/call.ts
+++ lib/call.ts
@@ -1,12 +1,77 @@
 import type { Operation } from "./types.ts";
 import { action } from "./instructions.ts";
+import { pause } from "./pause.ts";
 
-export function call<T>(fn: () => Operation<T>): Operation<T> {
+/**
+ * Pause the current operation, and run a promises, async functions, or
+ * operations within a new scope. The calling operation will resumed (or errored)
+ * once call is completed.
+ *
+ * `call()` is a uniform integration point for calling async functions,
+ * evaluating promises; as well as generator functions and operations. It can be
+ * used to treat a promise as an operation.
+ *
+ * @example
+ * ```js
+ * let response = yield* call(fetch('https://google.com'));
+ * ```
+ * or an async function:
+ *
+ * @example
+ * '''ts
+ * async function* googleSlowly() {
+ *   return yield* call(async function() {
+ *     await new Promise(resolve => setTimeout(resolve, 2000));
+ *     return await fetch("https://google.com");
+ *   });
+ * }
+ * ``'
+ *
+ * It can be used to run an operation in a separate scope to ensure that any
+ * resources allocated:
+ *
+ * @example
+ * ```js
+ * yield* call(function*() {
+ *   let socket = yield* useSocket();
+ *   return yield* socket.read();
+ * }); //=> socket is destroyed before returning
+ * ```
+ *
+ * Because `call()` runs within its own {@link Scope}, it can also be used to
+ * establish {@link  * establish error boundarieshttps://frontside.com/effection/docs/errors | error boundaries}.
+ *
+ * @param operator the operation, promise, async function, or generator funnction to call as part of this operation
+ */
+export function call<T>(operator: () => Operation<T>): Operation<T>;
+export function call<T>(operator: () => Promise<T>): Operation<T>;
+export function call<T>(operator: Operation<T>): Operation<T>;
+export function call<T>(operator: Promise<T>): Operation<T>;
+export function call<T>(operator: Operator<T>): Operation<T> {
   return action(function* (resolve, reject) {
     try {
-      resolve(yield* fn());
+      let op = typeof operator === "function" ? operator() : operator;
+      if (typeof (op as Operation<T>)[Symbol.iterator] === "function") {
+        resolve(yield* op);
+      } else {
+        //@ts-expect-error this is because we monkey-patch the tests
+        resolve(yield* expect(op));
+      }
     } catch (error) {
       reject(error);
     }
   });
 }
+
+function expect<T>(promise: Promise<T>): Operation<T> {
+  return pause((resolve, reject) => {
+    promise.then(resolve, reject);
+    return () => {};
+  });
+}
+
+type Operator<T> =
+  | Operation<T>
+  | Promise<T>
+  | (() => Operation<T>)
+  | (() => Promise<T>);
--- test/call.test.ts
+++ test/call.test.ts
@@ -3,13 +3,33 @@ import { describe, expect, it } from "./suite.ts";
 import { call, run, spawn, suspend } from "../mod.ts";
 
 describe("call", () => {
-  it("invokes an operation", async () => {
+  it("evaluates an operation function", async () => {
     await run(function* () {
       let result = yield* call(function* () {
         return 10;
       });
       expect(result).toEqual(10);
     });
+  });
+
+  it("evaluates an operation directly", async () => {
+    await expect(run(() =>
+      call({
+        *[Symbol.iterator]() {
+          return 42;
+        },
+      })
+    )).resolves.toEqual(42);
+  });
+
+  it("evaluates a no-arg async function", async () => {
+    await expect(run(() => call(() => Promise.resolve(42)))).resolves.toEqual(
+      42,
+    );
+  });
+
+  it("evaluates a promise directly", async () => {
+    await expect(run(() => call(Promise.resolve(42)))).resolves.toEqual(42);
   });
 
   it("can be used as an error boundary", async () => {

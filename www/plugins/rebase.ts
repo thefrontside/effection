43c3291e6ef09091bb99beb685186f7de13f5f03
Charles Lowell <cowboyd@frontside.com>
2023-11-14 17:00:37 -0600

    Upgrade to revolution 0.3.0
    The middleware stack was not clear exactly what was the list of routes
    that would run. Before they were `jsx` -> `html` -> `http`, but that
    made it look like the stack would run every time, but in fact, both
    `jsx` and `html` were really the same thing, where `jsx` was the input
    to the `html` middleware.
    
    This flattens the entire "stack" into a single list of top level
    "middlewares". If the middleware returns a JSX element, then, and only
    then, will the HTML middleware transform it. But, its place in the
    overall http app is the same. Because they are all in line now, it is
    now clear what the precedence rules are (0 -> n) because all
    middlewares are in a single list.

---
+++ www/plugins/rebase.ts
@@ -0,0 +1,74 @@
+import type { RevolutionPlugin } from "revolution";
+import { createContext, type Operation } from "effection";
+import { posixNormalize } from "https://deno.land/std@0.203.0/path/_normalize.ts";
+import { selectAll } from "npm:hast-util-select@6.0.1";
+
+const BaseUrl = createContext<URL>("baseUrl");
+const CurrentRequest = createContext<Request>("Request");
+
+export function rebasePlugin(): RevolutionPlugin {
+  return {
+    *http(request, next) {
+      yield* CurrentRequest.set(request);
+
+      let rebaseUrl = request.headers.get("X-Base-Url") ?? void 0;
+      if (rebaseUrl) {
+        yield* BaseUrl.set(new URL(rebaseUrl));
+      } else {
+        let url = new URL(request.url);
+        url.pathname = "/";
+        yield* BaseUrl.set(url);
+      }
+
+      return yield* next(request);
+    },
+
+    /**
+     * Rebase an HTML document at a different URL. This replaces all `<a href>` and
+     * `<img src>` attributes that contain an absolute path. Any path that is
+     * relative or contains a fully qualitfied URL will be left alone.
+     *
+     * @param tree - the HTML tree to transform
+     * @param baseUrl - a string representing a fully qualified url, e.g.
+     *   http://frontside.com/effection
+     */
+    *html(request, next) {
+      let tree = yield* next(request);
+
+      let baseUrl = yield* BaseUrl;
+      let base = new URL(baseUrl);
+      let elements = selectAll('[href^="/"],[src^="/"]', tree);
+
+      for (let element of elements) {
+        let properties = element.properties!;
+
+        if (properties.href) {
+          properties.href = posixNormalize(
+            `${base.pathname}${properties.href}`,
+          );
+        }
+        if (properties.src) {
+          properties.src = posixNormalize(`${base.pathname}${properties.src}`);
+        }
+      }
+      return tree;
+    },
+  };
+}
+
+/**
+ * Convert a non fully qualified url into a fully qualified url, complete
+ * with protocol.
+ */
+export function* useAbsoluteUrl(path: string): Operation<string> {
+  let normalizedPath = posixNormalize(path);
+  if (normalizedPath.startsWith("/")) {
+    let base = yield* BaseUrl;
+    let url = new URL(base);
+    url.pathname = posixNormalize(`${base.pathname}${path}`);
+    return url.toString();
+  } else {
+    let request = yield* CurrentRequest;
+    return new URL(path, request.url).toString();
+  }
+}
--- www/routes/v2docs-route.tsx
+++ www/routes/v2docs-route.tsx
@@ -2,31 +2,28 @@ import type { HTTPMiddleware } from "revolution";
 import type { V2Docs } from "../docs/v2-docs.ts";
 
 import { call } from "effection";
-import { route } from "revolution";
+import { concat, route } from "revolution";
 import { serveTar } from "./serve-tar.ts";
 
 export function v2docsRoute(v2docs: V2Docs): HTTPMiddleware {
   let { apidocs, website } = v2docs;
 
-  let api = route("/V2/api(.*)", function* (request) {
-    return yield* call(serveTar(request, {
-      tarRoot: "api/v2",
-      urlRoot: "V2/api",
-      entries: yield* apidocs,
-    }));
-  });
-
-  let docs = route("/V2(.*)", function* (request) {
-    return yield* call(serveTar(request, {
-      tarRoot: "site",
-      urlRoot: "V2",
-      entries: yield* request.headers.has("X-Base-Url")
-        ? website.prod
-        : website.local,
-    }));
-  });
-
-  return function* (request, next) {
-    return yield* api(request, (request) => docs(request, next));
-  };
+  return concat(
+    route("/V2/api(.*)", function* (request) {
+      return yield* call(serveTar(request, {
+        tarRoot: "api/v2",
+        urlRoot: "V2/api",
+        entries: yield* apidocs,
+      }));
+    }),
+    route("/V2(.*)", function* (request) {
+      return yield* call(serveTar(request, {
+        tarRoot: "site",
+        urlRoot: "V2",
+        entries: yield* request.headers.has("X-Base-Url")
+          ? website.prod
+          : website.local,
+      }));
+    }),
+  );
 }

524a1b2e05d5bccb77a5e5b1923699277bbeb79b
Taras Mankovski <taras@frontside.com>
2023-12-15 17:10:00 -0500

    Created footer component

--- www/routes/useAppHtml.tsx
+++ www/routes/useAppHtml.tsx
@@ -1,22 +1,16 @@
 import type { Operation } from "effection";
 import type { JSXChild } from "revolution";
-
 import { useAbsoluteUrl } from "../plugins/rebase.ts";
 import { ProjectSelect } from "../components/project-select.tsx";
-import { Footer } from "../components/footer.tsx";
-import { IconGithHub } from "../components/icons/github.tsx";
-import { IconDiscord } from "../components/icons/discord.tsx";
+import { Options, IconGithHub, IconDiscord, IconExtern } from "./app.html.tsx";
 
-export interface Options {
-  title: string;
-}
 
 export function* useAppHtml(
-  { title }: Options,
-): Operation<({ children }: { children: JSXChild }) => JSX.Element> {
+  { title }: Options
+): Operation<({ children }: { children: JSXChild; }) => JSX.Element> {
   let homeURL = yield* useAbsoluteUrl("/");
   let twitterImageURL = yield* useAbsoluteUrl(
-    "/assets/images/meta-effection.png",
+    "/assets/images/meta-effection.png"
   );
 
   return ({ children }) => (
@@ -25,55 +19,45 @@ export function* useAppHtml(
         <meta charset="UTF-8" />
         <title>{title}</title>
         <meta property="og:image" content="/assets/images/meta-effection.png" />
-        <meta
+        <meta 
           property="og:title"
           content="Introduction | Effection"
-          data-rh="true"
-        />
-        <meta
+          data-rh="true" />
+        <meta 
           property="og:url"
-          content={homeURL}
-        />
-        <meta
+          content={homeURL} />
+        <meta 
           property="og:description"
-          content="Effection is a structured concurrency and effects framework for JavaScript."
-        />
-        <meta
+          content="Effection is a structured concurrency and effects framework for JavaScript." />
+        <meta 
           name="description"
-          content="Effection is a structured concurrency and effects framework for JavaScript."
-        />
-        <meta
+          content="Effection is a structured concurrency and effects framework for JavaScript." />
+        <meta 
           name="twitter:image"
-          content={twitterImageURL}
-        />
+          content={twitterImageURL} />
         <link rel="icon" href="/assets/images/favicon-effection.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
+        <link 
           rel="canonical"
-          href={homeURL}
-        />
-        <link
+          href={homeURL} />
+        <link 
           rel="alternate"
           href={homeURL}
-          hreflang="en"
-        />
-        <link
+          hreflang="en" />
+        <link 
           rel="alternate"
           href={homeURL}
-          hreflang="x-default"
-        />
-        <link
+          hreflang="x-default" />
+        <link 
           href="/assets/prism-atom-one-dark.css"
           rel="preload"
           as="style"
-          onload="this.rel='stylesheet'"
-        />
-        <link
+          onload="this.rel='stylesheet'" />
+        <link 
           href="https://use.typekit.net/ugs0ewy.css"
           rel="preload"
           as="style"
-          onload="this.rel='stylesheet'"
-        />
+          onload="this.rel='stylesheet'" />
         <noscript>
           <link rel="stylesheet" href="https://use.typekit.net/ugs0ewy.css" />
           <link rel="stylesheet" href="/assets/prism-atom-one-dark.css" />
@@ -83,16 +67,15 @@ export function* useAppHtml(
         <header class="header w-full top-0 p-6 sticky tracking-wide z-10">
           <div class="flex items-center justify-between">
             <div>
-              <a
+              <a 
                 href="/"
                 class="flex items-end gap-x-2 sm:gap-x-1 after:content-['v3'] after:inline after:relative after:top-0 after:text-sm"
               >
-                <img
+                <img 
                   src="/assets/images/effection-logo.svg"
                   alt="Effection Logo"
                   width="156px"
-                  height="24px"
-                />
+                  height="24px" />
               </a>
             </div>
 
@@ -130,7 +113,45 @@ export function* useAppHtml(
         <main>
           {children}
         </main>
-        <Footer />
+        <footer class="grid grid-cols-3 text-center text-gray-500 tracking-wide bg-gray-100 py-10 gap-y-4 leading-10">
+          <section class="flex flex-col gap-y-1">
+            <h1 class="text-sm uppercase font-bold text-blue-primary mb-4">
+              About
+            </h1>
+            <a class="text-gray-800" href="https://frontside.com">
+              Maintained by Frontside <IconExtern />
+            </a>
+          </section>
+          <section class="flex flex-col gap-y-1">
+            <h1 class="text-sm uppercase font-bold text-blue-primary mb-4">
+              OSS Projects
+            </h1>
+            <a href="https://frontside.com/interactors" class="text-gray-800">
+              Interactors <IconExtern />
+            </a>
+            <a href="/V2" class="text-gray-800">
+              Effection<em class="align-super text-xs">v2</em> <IconExtern />
+            </a>
+          </section>
+          <section class="flex flex-col gap-y-1">
+            <h1 class="text-sm uppercase font-bold text-blue-primary mb-4">
+              Community
+            </h1>
+            <a href="https://discord.gg/r6AvtnU" class="text-gray-800">
+              Discord <IconExtern />
+            </a>
+            <a 
+              href="https://github.com/thefrontside/effection"
+              class="text-gray-800"
+            >
+              GitHub <IconExtern />
+            </a>
+          </section>
+          <p class="col-span-3 text-blue-primary text-xs py-4">
+            Copyright © 2019 - {new Date().getFullYear()}{" "}
+            The Frontside Software, Inc.
+          </p>
+        </footer>
       </body>
     </html>
   );

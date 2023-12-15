379102bbb760045823dc87d4dc893eeeaea44aeb
Charles Lowell <cowboyd@frontside.com>
2023-12-15 11:23:00 -0600

    🔥Remove deprerated `expect()`
    No need shipping a deprecated function in the 3.0 release

--- lib/async.ts
+++ lib/async.ts
@@ -1,16 +1,6 @@
-import type { Operation, Stream, Subscription } from "./types.ts";
+import type { Stream, Subscription } from "./types.ts";
 
-import { action } from "./instructions.ts";
 import { call } from "./call.ts";
-
-/**
- * @deprecated use {@link call} instead
- */
-export function expect<T>(promise: Promise<T>): Operation<T> {
-  return action(function* (resolve, reject) {
-    promise.then(resolve, reject);
-  });
-}
 
 export function subscribe<T, R>(iter: AsyncIterator<T, R>): Subscription<T, R> {
   return {

95216e69030502035f6e691cc70a330cdeff851b
Taras Mankovski <taras@frontside.com>
2024-02-16 10:17:55 -0500

    Added walk command

--- www/lib/useDeno.ts
+++ www/lib/useDeno.ts
@@ -1,21 +1,24 @@
-import { call, Operation } from 'effection';
+import { walk } from "https://deno.land/std@0.216.0/fs/walk.ts";
+import { call, Operation } from "effection";
 import { CwdContext } from "./useCwd.ts";
 import { MakeTmpDirContext } from "./useTmpCwd.ts";
 import { ReadDirContext } from "./useReadDir.ts";
 import { MkdirContext } from "./useMkdir.ts";
 import { EnvContext } from "./useEnv.ts";
+import { WalkContext } from "./useWalk.ts";
 
 export function withDeno<T>(op: () => Operation<T>) {
   return call(function* () {
-    yield* useDeno();
+    yield* initDeno();
     return yield* op();
   });
 }
 
-export function* useDeno() {
+export function* initDeno() {
   yield* CwdContext.set(Deno.cwd);
   yield* MakeTmpDirContext.set(Deno.makeTempDir);
   yield* ReadDirContext.set(Deno.readDir);
   yield* MkdirContext.set(Deno.mkdir);
   yield* EnvContext.set(Deno.env);
+  yield* WalkContext.set(walk);
 }

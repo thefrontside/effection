0541c334436defbbb58caa1a6944b4079ea52965
Charles Lowell <cowboyd@frontside.com>
2024-11-05 10:08:06 -0600

    ✨Introduce Do() operation
    Many of the Operations are comprised of a single Effect, and very
    often a synchronous Effect at that. There is no need to pay any of the
    overhead for creating a generator which has, among other things its
    own independent stack frame, when we could just make a simple
    iterator.
    
    This introduces a `Do()` operation which takes any effect and converts
    it automatically into an operation where it can be used anywhere. This
    gives us better performance and also solves the circular dependency
    problem inside `context.ts`

---
+++ lib/do.ts
@@ -0,0 +1,35 @@
+import { Result } from "./result.ts";
+import { Effect, Operation } from "./types.ts";
+
+/**
+ * Perform a single Effect as an Operation
+ */
+export function Do<T>(effect: Effect<T>): Operation<T> {
+  return {
+    [Symbol.iterator]() {
+      let result: Result<T> | undefined = undefined;
+      let perform: Effect<T> = {
+        description: `do <${effect.description}>`,
+        enter(resolve, routine) {
+          return effect.enter((r) => {
+            resolve(result = r);
+          }, routine);
+        },
+      };
+
+      return {
+        next() {
+          if (result) {
+            if (result.ok) {
+              return { done: true, value: result.value };
+            } else {
+              throw result.error;
+            }
+          } else {
+            return { done: false, value: perform };
+          }
+        },
+      };
+    },
+  };
+}

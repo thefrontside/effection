b220d5a11c037f6ea779d88c6260d12cb4d7458d
Taras Mankovski <taras@frontside.com>
2024-12-20 15:27:18 -0500

    Change only version

--- tasks/build-jsr.ts
+++ tasks/build-jsr.ts
@@ -4,12 +4,6 @@ await Deno.writeTextFile(
   new URL("../deno.json", import.meta.url),
   JSON.stringify({
     ...jsonDeno,
-    name: "@effection/effection",
     version: Deno.env.get("VERSION"),
-    exports: "./mod.ts",
-    license: "ISC",
-    publish: {
-      include: ["lib", "mod.ts", "README.md"],
-    },
   }),
 );

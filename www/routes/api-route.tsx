fdf59bdad273b67f09c3d095ce4508e06ec30ab3
Taras Mankovski <taras@frontside.com>
2024-12-28 21:55:30 -0500

    Improve interface arguments output

--- www/routes/api-route.tsx
+++ www/routes/api-route.tsx
@@ -83,30 +83,15 @@ export function apiRoute(): SitemapRoute<JSXElement> {
           <section class="min-h-0 mx-auto w-full justify-items-normal md:grid md:grid-cols-[225px_auto] lg:grid-cols-[225px_auto_200px] md:gap-4">
             <aside class="min-h-0 overflow-auto hidden md:block pt-2 top-24 sticky h-fit">
               <nav class="pl-4">
-                {nodesByKind.function ? (
-                  <hgroup class="mb-2">
-                    <h3 class="text-lg">Functions</h3>
-                    <Menu nodes={nodesByKind.function} current={symbol} />
-                  </hgroup>
-                ) : (
-                  <></>
-                )}
-                {nodesByKind.interface ? (
-                  <hgroup class="mb-2">
-                    <h3 class="text-lg">Interfaces</h3>
-                    <Menu nodes={nodesByKind.interface} current={symbol} />
-                  </hgroup>
-                ) : (
-                  <></>
-                )}
-                {nodesByKind.typeAlias ? (
-                  <hgroup class="mb-2">
-                    <h3 class="text-lg">Type Aliases</h3>
-                    <Menu nodes={nodesByKind.typeAlias} current={symbol} />
-                  </hgroup>
-                ) : (
-                  <></>
-                )}
+                <h3 class="text-lg">API Reference</h3>
+                <Menu
+                  nodes={{
+                    ...nodesByKind.typeAlias,
+                    ...nodesByKind.function,
+                    ...nodesByKind.interface,
+                  }}
+                  current={symbol}
+                />
               </nav>
             </aside>
             <article class="prose max-w-full px-6 py-2">{elements}</article>
@@ -126,17 +111,19 @@ function Menu({
 }) {
   return (
     <menu>
-      {Object.keys(nodes).map((name) => (
+      {Object.keys(nodes).sort().map((name) => (
         <li>
           {current === name ? (
-            <span class="rounded px-4 block w-full py-2 bg-gray-100 cursor-default ">
+            <span class="rounded px-2 block w-full py-2 bg-gray-100 cursor-default ">
+              <Icon node={nodes[name][0]} />
               {name}
             </span>
           ) : (
             <a
-              class="rounded px-4 block w-full py-2 hover:bg-gray-100"
+              class="rounded px-2 block w-full py-2 hover:bg-gray-100"
               href={`/api/${name}`}
             >
+              <Icon node={nodes[name][0]} />
               {name}
             </a>
           )}
@@ -144,4 +131,16 @@ function Menu({
       ))}
     </menu>
   );
+}
+
+function Icon({ node }: { node: RenderableDocNode }) {
+  switch(node.kind) {
+    case "function":
+      return <span class="rounded-full bg-sky-100 inline-block w-6 h-full mr-1 text-center">f</span>
+    case "interface":
+      return <span class="rounded-full bg-orange-50 text-orange-600 inline-block w-6 h-full mr-1 text-center">I</span>
+    case "typeAlias":
+      return <span class="rounded-full bg-red-50 text-red-600 inline-block w-6 h-full mr-1 text-center">T</span>
+  }
+  return <></>
 }

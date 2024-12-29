68aae177cec1a6f18a084220c0a972bdc88356c5
Taras Mankovski <taras@frontside.com>
2024-12-28 20:32:37 -0500

    Show a menu of all api exports

--- www/routes/api-route.tsx
+++ www/routes/api-route.tsx
@@ -1,16 +1,16 @@
 import denoJson from "../../deno.json" with { type: "json" };
 
-import type { JSXElement } from "revolution";
 import { call } from "effection";
-
-import { useAppHtml } from "./app.html.tsx";
-import { SitemapRoute } from "../plugins/sitemap.ts";
+import { Type } from "effection-contrib/www/components/api.tsx";
 import {
   createPackage,
-  initPackageContext,
+  type RenderableDocNode,
 } from "effection-contrib/www/hooks/use-package.tsx";
-import { API } from "effection-contrib/www/components/api.tsx";
+import { aggregateGroups } from "jsr:@std/collections@1.0.9";
+import { type JSXElement } from "revolution";
 
+import { SitemapRoute } from "../plugins/sitemap.ts";
+import { useAppHtml } from "./app.html.tsx";
 import { navLinks } from "./index-route.tsx";
 
 function* effectionPkgConfig() {
@@ -22,6 +22,9 @@ function* effectionPkgConfig() {
   };
 }
 
+const uniquePredicate = (value: unknown, index: number, array: unknown[]) =>
+  array.indexOf(value) === index;
+
 export function apiRoute(): SitemapRoute<JSXElement> {
   return {
     *routemap(generate) {
@@ -30,7 +33,7 @@ export function apiRoute(): SitemapRoute<JSXElement> {
 
       return pkg.docs["."]
         .map((node) => node.name)
-        .filter((value, index, array) => array.indexOf(value) === index)
+        .filter(uniquePredicate)
         .flatMap((symbol) => {
           return [
             {
@@ -42,13 +45,103 @@ export function apiRoute(): SitemapRoute<JSXElement> {
     handler: function* () {
       const AppHtml = yield* useAppHtml({ title: "API Reference " });
 
-      yield* initPackageContext(yield* effectionPkgConfig());
+      const config = yield* effectionPkgConfig();
+      const pkg = yield* createPackage(config);
+      const nodes = pkg.docs["."].filter((node) => node.name === symbol);
+      const nodesByKind = aggregateGroups<
+        RenderableDocNode,
+        Record<string, RenderableDocNode[]>
+      >(
+        Object.groupBy(pkg.docs["."], (node) => node.kind),
+        (current, _key, _first, accumulator = {}) => ({
+          ...accumulator,
+          ...{
+            [current.name]:
+              current.name in accumulator
+                ? [...accumulator[current.name]]
+                : [current],
+          },
+        }),
+      );
+
+      const elements: JSXElement[] = [];
+      for (const node of nodes) {
+        const { MDXDoc = () => <></> } = node;
+
+        elements.push(
+          <section id={node.id}>
+            {yield* Type({ node })}
+            <div class="pl-2 -mt-5">
+              <MDXDoc />
+            </div>
+          </section>,
+        );
+      }
 
       return (
         <AppHtml navLinks={navLinks}>
-          <>{yield* API()()}</>
+          <section class="min-h-0 mx-auto w-full justify-items-normal md:grid md:grid-cols-[225px_auto] lg:grid-cols-[225px_auto_200px] md:gap-4">
+            <aside class="min-h-0 overflow-auto hidden md:block pt-2 top-24 sticky h-fit">
+              <nav class="pl-4">
+                {nodesByKind.function ? (
+                  <hgroup class="mb-2">
+                    <h3 class="text-lg">Functions</h3>
+                    <Menu nodes={nodesByKind.function} current={symbol} />
+                  </hgroup>
+                ) : (
+                  <></>
+                )}
+                {nodesByKind.interface ? (
+                  <hgroup class="mb-2">
+                    <h3 class="text-lg">Interfaces</h3>
+                    <Menu nodes={nodesByKind.interface} current={symbol} />
+                  </hgroup>
+                ) : (
+                  <></>
+                )}
+                {nodesByKind.typeAlias ? (
+                  <hgroup class="mb-2">
+                    <h3 class="text-lg">Type Aliases</h3>
+                    <Menu nodes={nodesByKind.typeAlias} current={symbol} />
+                  </hgroup>
+                ) : (
+                  <></>
+                )}
+              </nav>
+            </aside>
+            <article class="prose max-w-full px-6 py-2">{elements}</article>
+          </section>
         </AppHtml>
       );
     },
   };
+}
+
+function Menu({
+  nodes,
+  current,
+}: {
+  current: string;
+  nodes: Record<string, RenderableDocNode[]>;
+}) {
+  return (
+    <menu>
+      {Object.keys(nodes).map((name) => (
+        <li>
+          {current === name ? (
+            <span class="rounded px-4 block w-full py-2 bg-gray-100 cursor-default ">
+              {name}
+            </span>
+          ) : (
+            <a
+              class="rounded px-4 block w-full py-2 hover:bg-gray-100"
+              href={`/api/${name}`}
+            >
+              {name}
+            </a>
+          )}
+        </li>
+      ))}
+    </menu>
+  );
 }

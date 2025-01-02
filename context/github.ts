889cf16e18ef9b517264ca5bf0228cab20135c95
Taras Mankovski <taras@frontside.com>
2025-01-02 13:13:28 -0500

    Caching github requests

--- context/github.ts
+++ context/github.ts
@@ -1,8 +1,25 @@
 import { createContext } from "effection";
+import { endpoint } from "npm:@octokit/endpoint@10.1.2";
 import { Octokit } from "npm:octokit@^4.0.0";
+import { CacheContext } from "./cache.ts";
 
 export const GithubClientContext = createContext<Octokit>("github-client");
 
 export function* initGithubClientContext({ token }: { token: string }) {
-  return yield* GithubClientContext.set(new Octokit({ token }))
+  const cache = yield* CacheContext.expect();
+
+  const octokit = new Octokit({ token });
+
+  octokit.hook.wrap("request", async (request, options) => {
+    const params = endpoint.parse(options);
+    
+    if (!cache.has(params.url)) {
+      const response = await request(options)
+      cache.set(params.url, response);
+    }
+
+    return cache.get(params.url);
+  })
+
+  return yield* GithubClientContext.set(octokit)
 }

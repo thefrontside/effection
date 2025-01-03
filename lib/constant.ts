035179d0224c6c9fc0ce71be3172717555d45018
Charles Lowell <cowboyd@frontside.com>
2025-01-03 14:56:47 -0600

    📝 document the `constant()` function

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,5 +1,17 @@
 import type { Operation } from "./types.ts";
 
+/**
+ * Create an {@link Operation} that always evaluates to `value`.
+ *
+ * @example
+ *
+ * ```ts
+ * let x = yield* constant("hello world");
+ * x === "hello world" //=> true
+ * ```
+ *
+ * @returns the {@link Operation} that evaluates to `value`;
+ */
 export function constant<T>(value: T): Operation<T> {
   return {
     [Symbol.iterator]: () => ({ next: () => ({ done: true, value }) }),

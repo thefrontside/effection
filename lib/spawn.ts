7cbf868ca8f0f86bb27c592330895ca03d27ca8d
Charles Lowell <cowboyd@frontside.com>
2025-01-09 09:27:38 -0600

    fix spawn docs, and add params to lift()

--- lib/spawn.ts
+++ lib/spawn.ts
@@ -6,8 +6,8 @@ import type { Effect, Operation, Task } from "./types.ts";
 /**
  * Run another operation concurrently as a child of the current one.
  *
- * The spawned operation will begin executing immediately and control will
- * return to the caller when it reaches its first suspend
+ * The spawned operation will begin executing at the next available
+ * opportunity.
  *
  * @example
  * ```js
@@ -26,29 +26,7 @@ import type { Effect, Operation, Task } from "./types.ts";
  * });
  * ```
  *
- * You should prefer using the spawn operation over calling
- * {@link Scope.run} from within Effection code. The reason being that a
- * synchronous failure in the spawned operation will not be caught
- * until the next yield point when using `run`, which results in lines
- * being executed that should not.
- *
- * @example
- * ```js
- * import { main, suspend, spawn, useScope } from 'effection';
- *
- * await main(function*() {
- *   yield* useScope();
- *
- *   scope.run(function*() {
- *    throw new Error('boom!');
- *   });
- *
- *   console.log('this code will run and probably should not');
- *
- *   yield* suspend(); // <- error is thrown after this.
- * });
- * ```
- * @param operation the operation to run as a child of the current task
+ * @param operation - the operation to run as a child of the current task
  * @typeParam T the type that the spawned task evaluates to
  * @returns a {@link Task} representing a handle to the running operation
  */

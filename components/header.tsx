7cd4dc0e857d5208f4dc0ac1ab6901ac5cd31b4b
Taras Mankovski <taras@frontside.com>
2025-01-25 18:22:50 -0500

    Fixed navigating to search

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,27 @@
-import { NavLinks } from "./nav-links.tsx";
+import { css } from "npm:@twind/core@1.1.3";
+
+import {
+  ContribRepositoryContext,
+  LibraryRepositoryContext,
+} from "../context/repository.ts";
+import { IconDiscord } from "./icons/discord.tsx";
+import { IconGithub } from "./icons/github.tsx";
+import { SearchIcon } from "./icons/search.tsx";
+import { StarIcon } from "./icons/star.tsx";
+
+const colorful = css`
+  background-image: linear-gradient(45deg, #f74d7b -20%, #8c7db3 95%);
+  &:hover {
+    background-image: linear-gradient(45deg, #8c7db3 -20%, #f74d7b 95%);
+  }
+`;
 
 export function* Header() {
+  let library = yield* LibraryRepositoryContext.expect();
+  let contrib = yield* ContribRepositoryContext.expect();
+  let contribMain = yield* contrib.loadRef();
+  let workspaces = yield* contribMain.loadWorkspaces();
+
   return (
     <header class="header w-full top-0 p-6 py-8 sticky tracking-wide z-10">
       <div class="flex items-center justify-between max-w-screen-2xl mx-auto">
@@ -15,9 +36,72 @@ export function* Header() {
           </a>
         </div>
         <nav aria-label="Site Nav" class="grow text-sm font-bold">
-          {yield* NavLinks()}
+          <>
+            <ul class="grid grid-flow-col auto-col-auto float-right place-items-end items-center gap-3 md:gap-7 lg:gap-12 md:ml-5">
+              <li>
+                <a href="/docs/installation">Guides</a>
+              </li>
+              <li>
+                <a href="/api">API</a>
+              </li>
+              <li class="hidden md:flex">
+                <a class="flex flex-row space-x-1" href="/contrib">
+                  <span>Contrib</span>
+                  <span>({workspaces.length})</span>
+                </a>
+              </li>
+              <li class="hidden md:flex">
+                <a
+                  class={`flex flex-row ${colorful} p-1 rounded`}
+                  href="https://github.com/thefrontside/effection"
+                >
+                  <span class="flex items-center">
+                    <IconGithub />
+                  </span>
+                  <span>GitHub</span>
+                  <span class="flex flex-row items-center pl-1 text-white">
+                    <StarIcon class="pr-0.5" />
+                    <span>{yield* library.starCount()}</span>
+                    <span class="font-black">+</span>
+                  </span>
+                </a>
+              </li>
+              <li class="hidden md:flex">
+                <a
+                  class="flex flex-row items-center space-x-1"
+                  href="https://discord.gg/r6AvtnU"
+                >
+                  <IconDiscord />
+                  <span>Discord</span>
+                </a>
+              </li>
+              <li class="hidden md:flex">
+                <form method="get" action="/search">
+                  <label class="h-9 w-[90px] relative block">
+                    <input
+                      id="search"
+                      type="search"
+                      name="q"
+                      class="relative block h-full w-full bg-slate-100 rounded-full text-slate-800 focus:outline-none focus:bg-white focus:border-slate-500 focus:ring-slate-500 focus:ring-2 pl-3 focus:w-[250px] focus:-ml-[160px] focus:z-1 transition-all text-lg"
+                      placeholder="⌘K"
+                    />
+                    <span class="absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SearchIcon class="w-6 mr-2 text-slate-400" />
+                    </span>
+                  </label>
+                </form>
+                <style>
+                  {`
+            input[type="search"]::-webkit-search-cancel-button {
+              -webkit-appearance: none;
+              appearance: none;
+            }
+          `}
+                </style>
+              </li>
+            </ul>
+          </>
         </nav>
-        {/* <div id="search"></div> */}
       </div>
     </header>
   );

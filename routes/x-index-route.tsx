23311022f738e257ad133ebc6ff6a4d91658f65a
Taras Mankovski <taras@frontside.com>
2025-04-10 19:59:41 -0400

    Cleaned up Extension page and added StarFX

--- routes/x-index-route.tsx
+++ routes/x-index-route.tsx
@@ -39,6 +39,8 @@ export function xIndexRoute({
       const ref = yield* x.loadRef();
       const packages = yield* ref.loadWorkspaces();
 
+      const makeChildUrl = createChildURL();
+
       return (
         <AppHTML search={search}>
           <article class="prose m-auto">
@@ -50,26 +52,60 @@ export function xIndexRoute({
               })}
             </header>
             <p>
-              Here are a list of community contributed modules that represent
-              emerging consensus on how to do common JavaScript tasks with
-              Effection.
+              A collection of reusable, community-created extensions - ranging
+              from small packages to complete frameworks - that show the best
+              practices for handling common JavaScript tasks with Effection.
             </p>
-            <ul class="list-none px-0">
-              {yield* all(
-                packages.map(function* (pkg) {
-                  return (
-                    <li class="px-0">
-                      <h3>
-                        <a href={yield* createChildURL()(pkg.path)}>
-                          {yield* pkg.title()}
+            <section class="ring-1 ring-slate-300 rounded">
+              <h2 class="p-4 bg-slate-100 mb-0 text-lg">Frameworks</h2>
+              <ul class="list-none px-0 divide-y-1  divide-solid">
+                <li>
+                  <a
+                    href="http://starfx.bower.sh"
+                    class="grid grid-flow-row no-underline pb-4 pt-4 px-4"
+                  >
+                    <span class="text-cyan-700 text-lg font-semibold">
+                      StarFX
+                    </span>
+                    <span>A micro-MVC framework for React App.</span>
+                  </a>
+                </li>
+              </ul>
+            </section>
+            <section class="ring-1 ring-slate-300 rounded">
+              <h2 class="p-4 bg-slate-100 mb-0 text-lg">Packages</h2>
+              <ul class="list-none px-0 divide-y-1  divide-solid">
+                {yield* all(
+                  packages.map(function* (pkg) {
+                    const [details] = yield* pkg.jsrPackageDetails();
+
+                    let title;
+                    let description;
+                    if (details.success) {
+                      title = `@${details.data.scope}/${details.data.name}`;
+                      description = details.data.description;
+                    } else {
+                      title = pkg.path;
+                      description = yield* pkg.description();
+                    }
+
+                    return (
+                      <li>
+                        <a
+                          href={yield* makeChildUrl(pkg.path)}
+                          class="grid grid-flow-row no-underline pb-4 pt-4 px-4"
+                        >
+                          <span class="text-cyan-700 text-lg font-semibold">
+                            {title}
+                          </span>
+                          <span class="">{description}</span>
                         </a>
-                      </h3>
-                      <p>{yield* pkg.description()}</p>
-                    </li>
-                  );
-                }),
-              )}
-            </ul>
+                      </li>
+                    );
+                  }),
+                )}
+              </ul>
+            </section>
           </article>
         </AppHTML>
       );

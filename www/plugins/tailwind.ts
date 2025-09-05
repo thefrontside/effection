b1b260c0e97d8fab7b95f8a7e4659828388d9ac7
Taras Mankovski <taras@frontside.com>
2025-09-04 22:37:38 -0400

    Use shell to start tailwind

--- www/plugins/tailwind.ts
+++ www/plugins/tailwind.ts
@@ -2,11 +2,11 @@ import { emptyDir, exists } from "jsr:@std/fs";
 import { join } from "jsr:@std/path";
 import { crypto } from "jsr:@std/crypto";
 import { encodeHex } from "jsr:@std/encoding/hex";
-import { x } from "jsr:@effection-contrib/tinyexec";
-import { call, Operation, until } from "effection";
+import { Operation, until } from "effection";
 import type { RevolutionPlugin } from "revolution";
 import { select } from "npm:hast-util-select";
-import { serveFile } from "https://deno.land/std@0.206.0/http/file_server.ts";
+import { serveFile } from "jsr:@std/http@1.0.20";
+import { $ } from "../context/shell.ts";
 
 export interface TailwindOptions {
   readonly input: string;
@@ -32,12 +32,12 @@ export function* tailwindPlugin(
       });
       return html;
     },
-    *http(request, next) {
+    http(request, next) {
       let url = new URL(request.url);
       if (url.pathname === css.csspath) {
-        return yield* call(() => serveFile(request, css.filepath));
+        return until(serveFile(request, css.filepath));
       } else {
-        return yield* next(request);
+        return next(request);
       }
     },
   };
@@ -51,35 +51,26 @@ interface CSS {
 
 function* compileCSS(options: TailwindOptions): Operation<CSS> {
   let { input, outdir } = options;
-  let outpath = join(outdir, input);
-  let proc = yield* x("deno", [
-    "run",
-    "--unstable-detect-cjs",
-    "-A",
-    "npm:@tailwindcss/cli@^4.0.0",
-    "--config",
-    "tailwind.config.ts",
-    "--input",
-    input,
-    "--output",
-    outpath,
-  ]);
-  let result = yield* proc;
-  if (result.stderr) {
-    console.log(result.stderr);
-  }
-  if (result.stdout) {
-    console.log(result.stdout);
-  }
-  if (yield* call(() => exists(outpath))) {
-    let content = yield* call(() => Deno.readFile(outpath));
-    const buffer = yield* call(() => crypto.subtle.digest("SHA-256", content));
+  let output = join(outdir, input);
+
+  const result = yield* $(
+`deno run -A \
+--unstable-detect-cjs \
+npm:@tailwindcss/cli@^4.0.0 \
+--config tailwind.config.ts \
+--input ${input} \
+--output ${output}`);
+
+  if (yield* until(exists(output))) {
+    let content = yield* until(Deno.readFile(output));
+    const buffer = yield* until(crypto.subtle.digest("SHA-256", content));
     const hash = encodeHex(buffer);
     return {
-      filepath: outpath,
-      csspath: `/${outpath}`,
-      href: `/${outpath}?${hash}`,
+      filepath: output,
+      csspath: `/${output}`,
+      href: `/${output}?${hash}`,
     };
   }
-  throw new Error(`failed to generate ${outpath}`);
+
+  throw new Error(`failed to generate ${output}`);
 }

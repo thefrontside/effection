6f342b72b609ee3d5b1cefc381a55d2533a74342
Taras Mankovski <taras@frontside.com>
2025-12-30 00:07:35 -0500

    Use h4 instead of h1

--- www/components/footer.tsx
+++ www/components/footer.tsx
@@ -4,9 +4,9 @@ export function Footer(): JSX.Element {
   return (
     <footer class="grid grid-cols-4 text-center text-gray-500 tracking-wide bg-gray-100 dark:bg-gray-900 dark:text-gray-400 py-8 gap-y-4 leading-8 justify-self-end px-5">
       <section class="flex flex-col gap-y-1">
-        <h1 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
+        <h4 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
           About
-        </h1>
+        </h4>
         <a
           class="text-gray-800 dark:text-gray-200"
           href="https://frontside.com"
@@ -21,9 +21,9 @@ export function Footer(): JSX.Element {
         </a>
       </section>
       <section class="flex flex-col gap-y-1">
-        <h1 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
+        <h4 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
           OSS Projects
-        </h1>
+        </h4>
         <a
           href="https://frontside.com/interactors"
           class="text-gray-800 dark:text-gray-200"
@@ -32,9 +32,9 @@ export function Footer(): JSX.Element {
         </a>
       </section>
       <section class="flex flex-col gap-y-1">
-        <h1 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
+        <h4 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
           Resources
-        </h1>
+        </h4>
         <a href="/assets/llms.txt" class="text-gray-800 dark:text-gray-200">
           LLM Reference
         </a>
@@ -43,9 +43,9 @@ export function Footer(): JSX.Element {
         </a>
       </section>
       <section class="flex flex-col gap-y-1">
-        <h1 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
+        <h4 class="text-sm uppercase font-bold text-blue-primary dark:text-blue-secondary mb-4">
           Community
-        </h1>
+        </h4>
         <a
           href="https://discord.gg/r6AvtnU"
           class="text-gray-800 dark:text-gray-200"

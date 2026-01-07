67cbefb1732ecbed1c9aa380a13b30f7e871e216
Charles Lowell <cowboyd@frontside.com>
2026-01-07 16:57:53 -0600

    make context api export explicit

--- lib/api/context.ts
+++ lib/api/context.ts
@@ -1,10 +1,14 @@
-import { createApi } from "../api.ts";
+import { type Api, createApi } from "../api.ts";
 import type { ScopeInternal } from "../scope-internal.ts";
 import type { Context, Scope } from "../types.ts";
+
+export interface ContextApi {
+  set<T>(scope: Scope, context: Context<T>, value: T): T;
+}
 
 export default createApi("Context", {
   set<T>(scope: Scope, context: Context<T>, value: T): T {
     let { contexts } = scope as ScopeInternal;
     return contexts[context.name] = value;
   },
-});
+}) as Api<ContextApi>;

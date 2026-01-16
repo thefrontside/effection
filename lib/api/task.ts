a3c6cae24208cfe5b8b2dbf843ff4031d07ea384
Charles Lowell <cowboyd@frontside.com>
2026-01-16 14:53:28 -0600

    get rid of halt() api until we can make some sense of it

--- lib/api/task.ts
+++ lib/api/task.ts
@@ -3,10 +3,8 @@ import type { Api, Operation } from "../types.ts";
 
 export interface TaskApi {
   run: <T>(operation: () => Operation<T>) => Operation<T>;
-  halt: (operation: () => Operation<void>) => Operation<void>;
 }
 
 export default createApi("Task", {
   run: (operation) => operation(),
-  halt: (operation) => operation(),
 }) as Api<TaskApi>;

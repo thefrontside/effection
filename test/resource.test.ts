thefrontside/effection

commit 153e4200879be327d04dc109a31aabcc3afb2c62

🐛 fix: destroy task scope when halting to prevent memory leak (#1081)
When a task was halted, the task itself would stop executing but its
associated scope remained alive, causing a memory leak.

This fix makes the `halt()` operation not do any teardown itself, but
instead it delegates to its own scope's `destroy()`. That way, not
only is the task execution halted, but every scope in its entire tree
is also destroyed. This ensures that all resources are cleaned up on
`Task.halt()`.

Note:

Fixing this did break one of the `each()` tests which
relied on this bugged behavior, so the implementation has been updated
to use a simple spawn instead of a resource.

Also, it broke a dubious behavior which is that a child could halt its
parent:

```ts
let task: Task<void> = run(function* root() {
  yield* sleep(0);
  yield* spawn(function* child() {
    yield* task.halt();
  });
  yield* suspend();
});
```

I believe that the intuitive behavior here is deadlock, which is what
now happens. In other words, the child computation uses scope hacking
to capture a reference to its parent, and then depends on that
parent's shutdown, which depends on waiting for the child to complete,
which requires the child to shutdown

I think we can put in a special guard for this at some point and at
least throw an exception here when we detect such an obvious deadlock,
but I don't think it should hold up fixing an egregious memory leak
such as this.

test/run.test.ts
@@ -3,13 +3,18 @@
 import { blowUp, createNumber, describe, expect, it } from "./suite.ts";
 import {
   action,
+  createScope,
   run,
+  type Scope,
   sleep,
   spawn,
   suspend,
   type Task,
   until,
+  useScope,
 } from "../mod.ts";
+import { Children } from "../lib/contexts.ts";
+import assert from "node:assert";
 
 describe("run()", () => {
   it("can run an operation", async () => {
@@ -207,7 +212,10 @@ describe("run()", () => {
     await expect(task).rejects.toMatchObject({ message: "halted" });
   });
 
-  it("can halt itself between yield points", async () => {
+  // TODO: this test is of dubious value. Deadlock might be the right thing here
+  // we can revisit and try to either detect this and deal with it, or maybe raise
+  // an error
+  it.skip("can halt itself between yield points", async () => {
     let task: Task<void> = run(function* root() {
       yield* sleep(0);
 
@@ -317,5 +325,19 @@ describe("run()", () => {
 
     await expect(task).rejects.toHaveProperty("message", "boom!");
     await expect(task.halt()).resolves.toBe(undefined);
+  });
+
+  it("destroys its scope when halted", async () => {
+    let scope: Scope | undefined;
+
+    let task = run(function* () {
+      scope = yield* useScope();
+      createScope(scope);
+      yield* suspend();
+    });
+    await task.halt();
+
+    assert(scope !== undefined);
+    expect(scope.expect(Children).size).toEqual(0);
   });
 });

lib/each.ts
@@ -1,9 +1,8 @@
-import type { Operation, Stream, Subscription } from "./types.ts";
 import { createContext } from "./context.ts";
-import { race } from "./race.ts";
-import { resource } from "./resource.ts";
-import { withResolvers } from "./with-resolvers.ts";
 import { useScope } from "./scope.ts";
+import { spawn } from "./spawn.ts";
+import type { Operation, Stream, Subscription } from "./types.ts";
+import { withResolvers } from "./with-resolvers.ts";
 
 /**
  * Consume an effection stream using a simple for-of loop.
@@ -36,9 +35,11 @@ export function each<T>(stream: Stream<T, unknown>): Operation<Iterable<T>> {
       if (!scope.hasOwn(EachStack)) {
         scope.set(EachStack, []);
       }
-      return yield* resource(function* (provide) {
-        let done = withResolvers<void>();
 
+      let done = withResolvers<void>();
+      let cxt = withResolvers<EachLoop<T>>();
+
+      yield* spawn(function* () {
         let subscription = yield* stream;
         let current = yield* subscription.next();
 
@@ -56,7 +57,15 @@ export function each<T>(stream: Stream<T, unknown>): Operation<Iterable<T>> {
 
         stack.push(context);
 
-        let iterator: Iterator<T> = {
+        cxt.resolve(context);
+
+        yield* done.operation;
+      });
+
+      let context = yield* cxt.operation;
+
+      return {
+        [Symbol.iterator]: () => ({
           next() {
             if (context.stale) {
               let error = new Error(
@@ -73,15 +82,8 @@ export function each<T>(stream: Stream<T, unknown>): Operation<Iterable<T>> {
             context.finish();
             return { done: true, value: void 0 };
           },
-        };
-
-        yield* race([
-          done.operation,
-          provide({
-            [Symbol.iterator]: () => iterator,
-          }),
-        ]);
-      });
+        }),
+      };
     },
   };
 }

lib/task.ts
@@ -23,18 +23,19 @@ export interface NewTask<T> {
 
 export function createTask<T>(options: TaskOptions<T>): NewTask<T> {
   let { owner, operation } = options;
-  let [scope] = createScopeInternal(owner);
+  let [scope, destroy] = createScopeInternal(owner);
   let future = createFuture<T>();
 
   let top = new Delimiter<T>(() => encapsulate(operation));
   scope.set(DelimiterContext, top as Delimiter<unknown>);
 
-  let halt = function* halt() {
-    yield* top.close();
-    future.reject(new Error("halted"));
-  };
-
-  scope.ensure(halt);
+  scope.ensure(function* () {
+    try {
+      yield* top.close();
+    } finally {
+      future.reject(new Error("halted"));
+    }
+  });
 
   let task = Object.defineProperties(future.future, {
     halt: {
@@ -43,24 +44,24 @@ export function createTask<T>(options: TaskOptions<T>): NewTask<T> {
         return Object.defineProperties(Object.create(Promise.prototype), {
           [Symbol.iterator]: {
             enumerable: false,
-            value: halt,
+            value: destroy,
           },
           then: {
             enumerable: false,
             value(...args: Parameters<Promise<void>["then"]>) {
-              return owner.run(halt).then(...args);
+              return owner.run(destroy).then(...args);
             },
           },
           catch: {
             enumerable: false,
             value(...args: Parameters<Promise<void>["catch"]>) {
-              return owner.run(halt).catch(...args);
+              return owner.run(destroy).catch(...args);
             },
           },
           finally: {
             enumerable: false,
             value(...args: Parameters<Promise<void>["finally"]>) {
-              return owner.run(halt).finally(...args);
+              return owner.run(destroy).finally(...args);
             },
           },
         });

commit cd009647259ca3a71d4c894ac173698a39ea2e1d

🐛 fix resource destruction order (#1091)
There was a regression introduced with
https://github.com/thefrontside/effection/pull/1081 such that when a task was
halted, its resources were being shut down _before_ the task body was
finished. This resulted in violation of the structured concurrency
guarantees that required everything in scope to remain alive until
after the body of the task is fully executed. This is because the body
of the operation may use resources that are in scope throughout the
_entirety_ of the operation. For example, in the following code, the
finally block of the main operation requires the `child` task to be
fully complete.

```ts
let task = run(function* () {
  let synchronize = withResolvers<void>();
  let child = yield* spawn(() => synchronize.operation);
  try {
    yield* suspend();
  } finally {
    synchronize.resolve();
    yield* child;
  }
});

await task.halt() // deadlock!
```

Because the main task dependes on `child`, child needs to be active
and only halted _after_ the main task is complete, _not_ before. (in
this case the antecedent resource is a simple spawned `Task`, but it
could be anything; a database connection, file handle, whatever...).
Only after all references to the resource have become inactive can the
resource be destroyed.

When destroying a scope then, we need to first destroy oneself by
interrupting effect iteration, and only once that is complete, destroy
child scopes. This is accomplished by not reversing scope destructors.

It turns out this regression was actually being surfaced by the an
existing testcase which we commented out 😬. Another test has been
added to ensure the proper shutdown order is preserved when explicitly
halting.

> Note: in the future we will simplify this process even further by
removing the scope "destructor" mechanism altogether and replacing it
with middleware. This will replace fiddling with the order of
registration and execution with explicit declaration of sequence of

lib/scope-internal.ts
@@ -74,7 +74,7 @@ export function createScopeInternal(
     unbind();
     let outcome = Ok();
     try {
-      for (let destructor of [...destructors].reverse()) {
+      for (let destructor of destructors) {
         try {
           destructors.delete(destructor);
           yield* destructor();

test/resource.test.ts
@@ -107,6 +107,37 @@ describe("resource", () => {
 
     expect(sequence).toEqual([1, 2, 3, 4]);
   });
+
+  it("is released in the reverse order from which it was acquired", async () => {
+    let sequence: string[] = [];
+
+    await run(function* () {
+      yield* resource<void>(function* (provide) {
+        try {
+          yield* provide();
+        } finally {
+          sequence.push("first start");
+          yield* sleep(5);
+          sequence.push("first done");
+        }
+      });
+      yield* resource<void>(function* (provide) {
+        try {
+          yield* provide();
+        } finally {
+          sequence.push("second start");
+          yield* sleep(10);
+          sequence.push("second done");
+        }
+      });
+    });
+    expect(sequence).toEqual([
+      "second start",
+      "second done",
+      "first start",
+      "first done",
+    ]);
+  });
 });
 
 function createResource(container: State): Operation<State> {

test/run.test.ts
@@ -211,10 +211,7 @@ describe("run()", () => {
     await expect(task).rejects.toMatchObject({ message: "halted" });
   });
 
-  // TODO: this test is of dubious value. Deadlock might be the right thing here
-  // we can revisit and try to either detect this and deal with it, or maybe raise
-  // an error
-  it.skip("can halt itself between yield points", async () => {
+  it("can halt itself between yield points", async () => {
     let task: Task<void> = run(function* root() {
       yield* sleep(0);
 

test/spawn.test.ts
@@ -225,4 +225,34 @@ describe("spawn", () => {
 
     expect(order).toEqual(["zero", "one", "two"]);
   });
+
+  it("preserves all child tasks until after the parent operation passes out of scope", async () => {
+    let sequence: string[] = [];
+
+    let task = run(function* () {
+      yield* spawn(function* () {
+        try {
+          yield* suspend();
+        } finally {
+          sequence.push("first");
+        }
+      });
+      yield* spawn(function* () {
+        try {
+          yield* suspend();
+        } finally {
+          sequence.push("second");
+        }
+      });
+      try {
+        yield* suspend();
+      } finally {
+        sequence.push("parent");
+      }
+    });
+
+    await task.halt();
+
+    expect(sequence).toEqual(["parent", "second", "first"]);
+  });
 });
e10cd3afc0547639f2ede84fe9c04e4e3d54b59a
Charles Lowell <cowboyd@frontside.com>
2026-02-04 16:40:24 -0600

    ✨ Introduce context APIs for algebraic effects (#1101)
    One of the most powerful patterns that we've uncovered in the past
    couple of years of writing Effection code in production is the ability
    to contextualize an API so that they can be decorated in order to
    alter its behavior at any point. In other circles, this capability is
    known as "Algebraic Effects" and "Contextual Effects".
    
    With them, we can build all manner of constructs with a single primitive that
    would otherwise require many unique mechanisms. These include things
    like:
    
    - dependency injection
    - mocking inside tests with test doubles
    - adding instrumentation such as OTEL spans and metrics colleciton to
    - existing interfaces
    - wrapping stuff in database transactions
    
    This functionality was available as an external
    extension (https://frontside.com/effection/x/context-api), but the
    pattern has proven so powerful that we're bringing it directly into
    Effection core. Among other things, this will allow us to provide the
    type of orthogonal observability that we need to build the Effection
    inspector without having to change the library itself in order to
    accomodate it.
    
    This change brings the context API functionality directly into
    Effection. To create an API, call `createApi()` with the "core"
    functionality, where the "core" is how it will behave without any
    modification.
    
    ```ts
    // logging.ts
    export const Logging = createApi("Logging", {
      *log(...values: unknown[]) {
        console.log(...values);
      }
    })
    
    // export member operations so they can be use standalone
    export const { log } = Logging.operations;
    ```
    
    ```ts
    import { log } from "./logging.ts"
    
    export function* example() {
     // do stuff
     yield* log("just did stuff");
    }
    ```
    
    ```ts
    // Override it contextually only inside this scope
    yield* logging.around({
      *log(args, next) {
        yield* next(...args.map(v => `[CUSTOM] ${v}`));
      }
    });
    ```
    
    Apis can be enhanced directly from a `Scope` as well:
    
    ```ts
    import { Logging } from "./logging.ts";
    
    function enhanceLogging(scope: Scope) {
      scope.around(Logging, {
        *log(args, next) {
          yield* next(...args.map(v => `[CUSTOM] ${v}`));
        }
      });
    }
    
    ```
    
    As an example, and as the api necessary to implement the inspector,
    this provides a Scope api which provides instrumentation for core
    Effection operations. Such as create(), destroy(), set(), and delete()
    for scopes.

--- lib/scope-internal.ts
+++ lib/scope-internal.ts
@@ -1,3 +1,5 @@
+import type { ApiInternal } from "./api-internal.ts";
+import { api as effection } from "./api.ts";
 import { Children, Priority } from "./contexts.ts";
 import { createFuture } from "./future.ts";
 import { Err, Ok, unbox } from "./result.ts";
@@ -5,7 +7,28 @@ import { createTask } from "./task.ts";
 
 import type { Context, Operation, Scope, Task } from "./types.ts";
 
+const api = effection.Scope;
+
 export function createScopeInternal(
+  parent?: Scope,
+): [ScopeInternal, () => Operation<void>] {
+  if (!parent) {
+    let [global, destroy] = buildScopeInternal();
+    global.around(api, {
+      create([parent]) {
+        return buildScopeInternal(parent);
+      },
+    }, { at: "min" });
+    return [global, destroy] as const;
+  } else {
+    return api.invoke(parent, "create", [parent]) as [
+      ScopeInternal,
+      () => Operation<void>,
+    ];
+  }
+}
+
+export function buildScopeInternal(
   parent?: Scope,
 ): [ScopeInternal, () => Operation<void>] {
   let destructors = new Set<() => Operation<void>>();
@@ -21,7 +44,7 @@ export function createScopeInternal(
       return (contexts[context.name] ?? context.defaultValue) as T | undefined;
     },
     set<T>(context: Context<T>, value: T): T {
-      return contexts[context.name] = value;
+      return api.invoke(scope, "set", [scope, context, value]) as T;
     },
     expect<T>(context: Context<T>): T {
       let value = scope.get(context);
@@ -33,7 +56,7 @@ export function createScopeInternal(
       return value;
     },
     delete<T>(context: Context<T>): boolean {
-      return delete contexts[context.name];
+      return api.invoke(scope, "delete", [scope, context]);
     },
     hasOwn<T>(context: Context<T>): boolean {
       return !!Reflect.getOwnPropertyDescriptor(contexts, context.name);
@@ -49,9 +72,49 @@ export function createScopeInternal(
       };
     },
 
+    around<A extends {}>(
+      api: ApiInternal<A>,
+      ...params: Parameters<ApiInternal<A>["around"]>
+    ) {
+      let [around, options] = params;
+      if (!scope.hasOwn(api.context)) {
+        scope.set(api.context, { min: [], max: [] });
+      }
+
+      let { min, max } = scope.expect(api.context);
+
+      if (options?.at === "min") {
+        min.push(around);
+      } else {
+        max.push(around);
+      }
+
+      api.invalidate(scope);
+    },
+
     ensure(op: () => Operation<void>): () => void {
       destructors.add(op);
       return () => destructors.delete(op);
+    },
+
+    reduce<T, S>(
+      context: Context<T>,
+      fn: (sum: S, item: T) => S,
+      initial: S,
+    ): S {
+      let sum = initial;
+      let current = contexts;
+      while (current) {
+        if (Object.hasOwn(current, context.name)) {
+          let item = current[context.name] as T;
+          if (item) {
+            sum = fn(sum, item);
+          }
+        }
+
+        current = Object.getPrototypeOf(current);
+      }
+      return sum;
     },
   });
 
@@ -59,38 +122,41 @@ export function createScopeInternal(
   scope.set(Children, new Set());
   parent?.expect(Children).add(scope);
 
-  let destroy = function* (): Operation<void> {
-    destroy = () => destruction.future;
-
-    parent?.expect(Children).delete(scope);
-    unbind();
-    let outcome = Ok();
-    try {
-      while (destructors.size > 0) {
-        let current = [...destructors];
-        destructors.clear();
-        for (let destructor of current) {
-          try {
-            yield* destructor();
-          } catch (error) {
-            outcome = Err(error as Error);
-          }
-        }
-      }
-    } finally {
-      if (outcome.ok) {
-        destruction.resolve();
-      } else {
-        destruction.reject(outcome.error);
-      }
-    }
-
-    unbox(outcome);
-  };
-
+  let destroy = () => api.invoke(scope, "destroy", [scope]);
   let unbind = parent
     ? (parent as ScopeInternal).ensure(() => destroy())
     : () => {};
+
+  scope.around(api, {
+    *destroy(): Operation<void> {
+      destroy = () => destruction.future;
+
+      parent?.expect(Children).delete(scope);
+      unbind();
+      let outcome = Ok();
+      try {
+        while (destructors.size > 0) {
+          let current = [...destructors];
+          destructors.clear();
+          for (let destructor of current) {
+            try {
+              yield* destructor();
+            } catch (error) {
+              outcome = Err(error);
+            }
+          }
+        }
+      } finally {
+        if (outcome.ok) {
+          destruction.resolve();
+        } else {
+          destruction.reject(outcome.error);
+        }
+      }
+
+      unbox(outcome);
+    },
+  }, { at: "min" });
 
   return [scope, () => destroy()];
 }
@@ -98,4 +164,9 @@ export function createScopeInternal(
 export interface ScopeInternal extends Scope, AsyncDisposable {
   contexts: Record<string, unknown>;
   ensure(op: () => Operation<void>): () => void;
+  reduce<T, TSum>(
+    context: Context<T>,
+    fn: (sum: TSum, item: T) => TSum,
+    initial: TSum,
+  ): TSum;
 }

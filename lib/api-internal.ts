f0b227edd1624d887026492ca417ff7b74da965f
Charles Lowell <cowboyd@frontside.com>
2026-02-04 16:40:24 -0600

    ✨ Introduce context APIs for algebraic effects (#1101)
    One of the most powerful patterns that we've uncovered in the past
    couple of years of writing Effection code in production is the ability
    to contextualize an API so that they can be decorated in order to
    alter its behavior at any point. In other circles, this capability is
    known as "Algebraic Effects" and "Contextual Effects".
    
    With them, we can build all manner of constructs with a single primitive that
    would otherwise require many unique mechanisms. These include things
    like:
    
    - dependency injection
    - mocking inside tests with test doubles
    - adding instrumentation such as OTEL spans and metrics colleciton to
    - existing interfaces
    - wrapping stuff in database transactions
    
    This functionality was available as an external
    extension (https://frontside.com/effection/x/context-api), but the
    pattern has proven so powerful that we're bringing it directly into
    Effection core. Among other things, this will allow us to provide the
    type of orthogonal observability that we need to build the Effection
    inspector without having to change the library itself in order to
    accomodate it.
    
    This change brings the context API functionality directly into
    Effection. To create an API, call `createApi()` with the "core"
    functionality, where the "core" is how it will behave without any
    modification.
    
    ```ts
    // logging.ts
    export const Logging = createApi("Logging", {
      *log(...values: unknown[]) {
        console.log(...values);
      }
    })
    
    // export member operations so they can be use standalone
    export const { log } = Logging.operations;
    ```
    
    ```ts
    import { log } from "./logging.ts"
    
    export function* example() {
     // do stuff
     yield* log("just did stuff");
    }
    ```
    
    ```ts
    // Override it contextually only inside this scope
    yield* logging.around({
      *log(args, next) {
        yield* next(...args.map(v => `[CUSTOM] ${v}`));
      }
    });
    ```
    
    Apis can be enhanced directly from a `Scope` as well:
    
    ```ts
    import { Logging } from "./logging.ts";
    
    function enhanceLogging(scope: Scope) {
      scope.around(Logging, {
        *log(args, next) {
          yield* next(...args.map(v => `[CUSTOM] ${v}`));
        }
      });
    }
    
    ```
    
    As an example, and as the api necessary to implement the inspector,
    this provides a Scope api which provides instrumentation for core
    Effection operations. Such as create(), destroy(), set(), and delete()
    for scopes.

---
+++ lib/api-internal.ts
@@ -0,0 +1,142 @@
+// deno-lint-ignore-file ban-types no-explicit-any
+import { createContext } from "./context.ts";
+import { useScope } from "./scope.ts";
+import type {
+  Api,
+  Around,
+  Context,
+  Middleware,
+  Operation,
+  Scope,
+} from "./types.ts";
+import type { ScopeInternal } from "./scope-internal.ts";
+
+export interface ApiInternal<A> extends Api<A> {
+  context: Context<{
+    max: Partial<Around<A>>[];
+    min: Partial<Around<A>>[];
+  }>;
+  core: A;
+}
+
+export function createApiInternal<A extends {}>(
+  name: string,
+  core: A,
+): ApiInternal<A> {
+  let fields = Object.keys(core) as (keyof A)[];
+
+  let context = createContext(`api::${name}`) as ApiInternal<A>["context"];
+
+  let api: ApiInternal<A> = {
+    core,
+    context,
+    invoke: (scope, key, args) => {
+      let handle = createHandle(api, scope);
+      let member = handle[key];
+      if (typeof member === "function") {
+        return member(...args);
+      } else {
+        return member;
+      }
+    },
+    around: (decorator, options = { at: "max" }) => ({
+      *[Symbol.iterator]() {
+        let scope = yield* useScope();
+        scope.around(api, decorator, options);
+      },
+    }),
+    operations: fields.reduce((sum, field) => {
+      if (typeof core[field] === "function") {
+        return Object.assign(sum, {
+          [field]: (...args: any) => ({
+            *[Symbol.iterator]() {
+              let scope = yield* useScope();
+              let target = api.invoke(scope, field, args);
+
+              return isOperation(target) ? yield* target : target;
+            },
+          }),
+        });
+      } else {
+        return Object.assign(sum, {
+          [field]: {
+            *[Symbol.iterator]() {
+              let scope = yield* useScope();
+              let target = api.invoke(scope, field, [] as any);
+              return isOperation(target) ? yield* target : target;
+            },
+          },
+        });
+      }
+    }, {} as Api<A>["operations"]),
+  };
+  return api;
+}
+
+function createHandle<A extends {}>(api: ApiInternal<A>, scope: Scope): A {
+  let handle = Object.create(api.core);
+  let $scope = scope as ScopeInternal;
+  for (let key of Object.keys(api.core) as Array<keyof A>) {
+    let dispatch = (args: unknown[], next: (...args: unknown[]) => unknown) => {
+      let { min, max } = $scope.reduce(api.context, (sum, current) => {
+        let min = current.min.flatMap((around) =>
+          around[key] ? [around[key]] : []
+        );
+        let max = current.max.flatMap((around) =>
+          around[key] ? [around[key]] : []
+        );
+
+        sum.min.push(...min);
+        sum.max.unshift(...max);
+
+        return sum;
+      }, {
+        min: [] as Around<A>[typeof key][],
+        max: [] as Around<A>[typeof key][],
+      });
+
+      let stack = combine(
+        max.concat(min) as Middleware<unknown[], unknown>[],
+      );
+      return stack(args, next);
+    };
+
+    if (typeof api.core[key] === "function") {
+      handle[key] = (...args: unknown[]) =>
+        dispatch(args, api.core[key] as (...args: unknown[]) => unknown);
+    } else {
+      Object.defineProperty(handle, key, {
+        enumerable: true,
+        get() {
+          return dispatch([], () => api.core[key]);
+        },
+      });
+    }
+  }
+  return handle;
+}
+
+function isOperation<T>(
+  target: Operation<T> | T,
+): target is Operation<T> {
+  return target && !isNativeIterable(target) &&
+    typeof (target as Operation<T>)[Symbol.iterator] === "function";
+}
+
+function isNativeIterable(target: unknown): boolean {
+  return (
+    typeof target === "string" || Array.isArray(target) ||
+    target instanceof Map || target instanceof Set
+  );
+}
+
+function combine<TArgs extends unknown[], TReturn>(
+  middlewares: Middleware<TArgs, TReturn>[],
+): Middleware<TArgs, TReturn> {
+  if (middlewares.length === 0) {
+    return (args, next) => next(...args);
+  }
+  return middlewares.reduceRight((sum, middleware) => (args, next) =>
+    middleware(args, (...args) => sum(args, next))
+  );
+}

d4f93c3382128f51decc1b708d1e33edfaf762f4
Joshua Amaju <joshsemail4work@gmail.com>
2026-02-19 19:00:43 +0100

    refactor: simplify disposer retrieval in using() function

--- lib/using.ts
+++ lib/using.ts
@@ -54,7 +54,7 @@ export function* using<T extends Disposable | AsyncDisposable>(value: T) {
     try {
       yield* provide(value);
     } finally {
-      yield* call(() => disposer());
+      yield* call(() => disposer.bind(value)());
     }
   });
 }

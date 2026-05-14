a03cbb0b885b2b6fe5131cd40b3774b8c3b0c049
Charles Lowell <cowboyd@frontside.com>
2026-05-13 21:15:34 -0500

    ♻️ Invert reducer ↔ delimiter control
    In order to implement critical sections that cannot be interrupted and
    that are also able to implement sticky return, we need to make the
    behavior of the reducer more context sensitive.
    
    This drops the whole validation behavior and instead completely
    inverts control over iteration to the delimiter from the reducer. Now,
    the reducer becomes a pure iterator-stepping engine. The delimiter can
    tell it to use "next", "return", "throw" to tell it how to advance the
    iterator, or it can even tell it "drop" to ignore the dequeued item
    entirely.

--- lib/delimiter.ts
+++ lib/delimiter.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-unsafe-finally
 import { createContext } from "./context.ts";
-import { useCoroutine } from "./coroutine.ts";
+import { type StepType, useCoroutine } from "./coroutine.ts";
 import { Just, type Maybe, Nothing } from "./maybe.ts";
 import { Err, Ok, type Result } from "./result.ts";
 import type { Coroutine, Operation } from "./types.ts";
@@ -8,10 +8,10 @@ import { withResolvers } from "./with-resolvers.ts";
 
 export class Delimiter<T>
   implements Operation<Maybe<Result<T>>>, ErrorBoundary {
-  level = 0;
-  finalized = false;
-  future = withResolvers<Maybe<Result<T>>>();
+  state: "running" | "cancelling" | "finalized" = "running";
+  epoch = 0;
   computed = false;
+  future = withResolvers<Maybe<Result<T>>>();
   routine?: Coroutine;
   outcome?: Maybe<Result<T>>;
 
@@ -20,26 +20,38 @@ export class Delimiter<T>
     public readonly parent?: Delimiter<unknown>,
   ) {}
 
+  nextStep(result: Result<unknown>, epoch: number): StepType {
+    if (this.epoch !== epoch || this.state === "finalized") {
+      return "drop";
+    } else if (!result.ok) {
+      return "throw";
+    } else if (this.state === "cancelling") {
+      this.state = "running";
+      return "return";
+    } else {
+      return "next";
+    }
+  }
+
   raise(error: Error): void {
     let failure = Just(Err<T>(error));
-    if (this.finalized) {
-      this.parent?.exit(failure);
+    if (this.state === "finalized") {
+      this.parent?.signal(failure);
     } else {
-      this.exit(failure);
+      this.signal(failure);
     }
   }
 
   interrupt(): void {
-    this.exit(Nothing());
+    this.signal(Nothing());
   }
 
   *close(): Operation<void> {
     let done = this.future.operation;
-    let interrupted = !this.computed;
 
     this.close = function* close() {
       let outcome = yield* done;
-      if (interrupted && outcome.exists && !outcome.value.ok) {
+      if (this.epoch > 0 && outcome.exists && !outcome.value.ok) {
         throw outcome.value.error;
       }
     };
@@ -47,40 +59,31 @@ export class Delimiter<T>
       this.interrupt();
       yield* this.close();
     } else {
-      if (interrupted && this.outcome.exists && !this.outcome.value.ok) {
+      if (this.epoch > 0 && this.outcome.exists && !this.outcome.value.ok) {
         throw this.outcome.value.error;
       }
     }
   }
 
-  private exit(outcome: Maybe<Result<T>>): void {
-    if (this.finalized) {
-      return;
-    }
-    this.outcome =
-      (this.outcome && this.outcome.exists && !this.outcome.value.ok)
-        ? this.outcome
-        : outcome;
-    this.level++;
+  private signal(outcome: Maybe<Result<T>>): void {
+    if (this.state === "finalized" || this.epoch > 0) return;
+
+    this.outcome = outcome;
+    this.state = "cancelling";
+    this.epoch++;
     if (!this.routine) {
-      this.finalized = true;
+      this.state = "finalized";
       this.future.resolve(this.outcome);
     } else {
-      this.routine.return(Ok(this.outcome));
+      this.routine.next(Ok(this.outcome));
     }
-  }
-
-  get validator(): () => boolean {
-    let { level } = this;
-    return () => !this.finalized && this.level === level;
   }
 
   [Symbol.iterator] = function* delimiter(this: Delimiter<T>) {
-    this.routine = yield* useCoroutine();
-
     try {
+      this.routine = yield* useCoroutine();
       let value = yield* this.operation();
-      if (this.level === 0) {
+      if (this.epoch === 0) {
         this.computed = true;
         this.outcome = Just(Ok(value));
       }
@@ -88,7 +91,7 @@ export class Delimiter<T>
       this.computed = true;
       this.outcome = Just(Err(error as Error));
     } finally {
-      this.finalized = true;
+      this.state = "finalized";
       this.outcome = this.outcome ?? Nothing();
       this.future.resolve(this.outcome);
       return this.outcome;
